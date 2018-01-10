ebff50366b5076e9775a1aed96d5bb91a6f06ef4
Jakub Ziolkowski <Jakub.Ziolkowski@BuroHappold.com>
2018-01-10 10:44:09 +0000

    Structure updated to keep consistency with BHoM 2.0
    - Old code added to Legancy folder
    - Revit 2018 Adapter added (with CRUD)
    - Revit 2018 Engine added (with Convert)

---
+++ Revit2018_Adapter/CRUD/Read.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+
+using BH.oM.Base;
+
+namespace BH.Adapter.Revit
+{
+    public partial class RevitAdapter
+    {
+        public IEnumerable<BHoMObject> Read(Document Document, IEnumerable<BuiltInCategory> BuiltInCategories)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BHoMObject> Read(Document Document, IEnumerable<string> UniqueIds)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
