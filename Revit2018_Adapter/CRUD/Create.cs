99ae1c46a35d09c7ab49fff436262cf7d8834f86
Jakub Ziolkowski <Jakub.Ziolkowski@BuroHappold.com>
2018-01-18 16:09:34 +0000

    FromRevit and ToRevit functions updated

--- Revit2018_Adapter/CRUD/Create.cs
+++ Revit2018_Adapter/CRUD/Create.cs
@@ -4,10 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using BH.oM.Environmental.Elements;
+using BH.oM.Environmental.Properties;
+using BH.oM.Structural.Elements;
+using BH.Engine.Revit;
+using BH.Engine.Environment;
+
+using Autodesk.Revit.DB;
+
 namespace BH.Adapter.Revit
 {
     public partial class RevitAdapter
     {
+        public Building ReadBuilidng(Document Document, bool GenerateSpaces, bool Spaces3D)
+        {
+            Building aBuilding = Document.FromRevit();
 
+            List<BuildingElementProperties> aBuildingElementPropertiesList = new FilteredElementCollector(Document).OfClass(typeof(WallType)).ToList().ConvertAll(x => ((WallType)x).FromRevit());
+
+            List<Level> aLevelList = new FilteredElementCollector(Document).OfClass(typeof(Level)).Cast<Level>().ToList();
+            List<Storey> aStoreyList = new List<Storey>();
+            if (aLevelList != null && aLevelList.Count > 0)
+                aStoreyList = aLevelList.ConvertAll(x => x.FromRevit());
+
+            List<SpatialElement> aSpatialElementList = new FilteredElementCollector(Document).OfCategory(BuiltInCategory.OST_MEPSpaces).Cast<SpatialElement>().ToList();
+            aSpatialElementList.RemoveAll(x => x.Area < 0.001);
+
+            aBuilding.Add(aStoreyList);
+
+            if (GenerateSpaces)
+            {
+                SpatialElementBoundaryOptions aSpatialElementBoundaryOptions = new SpatialElementBoundaryOptions();
+                aSpatialElementBoundaryOptions.SpatialElementBoundaryLocation = SpatialElementBoundaryLocation.Center;
+                aSpatialElementBoundaryOptions.StoreFreeBoundaryFaces = true;
+
+                if (Spaces3D)
+                {
+                    SpatialElementGeometryCalculator aSpatialElementGeometryCalculator = new SpatialElementGeometryCalculator(Document, aSpatialElementBoundaryOptions);
+                    foreach (SpatialElement aSpatialElement in aSpatialElementList)
+                    {
+                        Space aSpace = aSpatialElement.FromRevit(aSpatialElementGeometryCalculator, aBuildingElementPropertiesList, aStoreyList);
+                        if (aSpace != null)
+                            aBuilding.Add(aSpace);
+                    }
+                }
+                else
+                {
+                    foreach (SpatialElement aSpatialElement in aSpatialElementList)
+                    {
+                        Space aSpace = aSpatialElement.FromRevit(aSpatialElementBoundaryOptions, aBuildingElementPropertiesList, aStoreyList);
+                        if (aSpace != null)
+                            aBuilding.Add(aSpace);
+                    }
+                }
+            }
+
+            return aBuilding;
+        }
     }
 }
--- Revit2018_Engine/Convert/ToRevit.cs
+++ Revit2018_Engine/Convert/ToRevit.cs
@@ -15,85 +15,119 @@ namespace BH.Engine.Revit
 {
     public static partial class Convert
     {
-        public static XYZ ToRevitPoint(this oM.Geometry.Point Point)
+        public static XYZ ToRevit(this oM.Geometry.Point Point)
         {
             return new XYZ(Point.X, Point.Y, Point.Z);
         }
 
-        public static XYZ ToRevitVector(this Vector Vector)
+        public static XYZ ToRevit(this Vector Vector)
         {
             return new XYZ(Vector.X, Vector.Y, Vector.Z);
         }
 
-        public static Curve ToRevitCurve(this ICurve ICurve)
+        public static Curve ToRevit(this ICurve ICurve)
         {
             if (ICurve is oM.Geometry.Line)
             {
                 oM.Geometry.Line aLine = ICurve as oM.Geometry.Line;
-                return Autodesk.Revit.DB.Line.CreateBound(ToRevitPoint(aLine.Start), ToRevitPoint(aLine.End));
+                return Autodesk.Revit.DB.Line.CreateBound(ToRevit(aLine.Start), ToRevit(aLine.End));
             }
 
             if (ICurve is oM.Geometry.Arc)
             {
                 oM.Geometry.Arc aArc = ICurve as oM.Geometry.Arc;
-                return Autodesk.Revit.DB.Arc.Create(ToRevitPoint(aArc.Start), ToRevitPoint(aArc.End), ToRevitPoint(aArc.Middle));
+                return Autodesk.Revit.DB.Arc.Create(ToRevit(aArc.Start), ToRevit(aArc.End), ToRevit(aArc.Middle));
             }
 
             if (ICurve is NurbCurve)
             {
                 NurbCurve aNurbCurve = ICurve as NurbCurve;
-                return NurbSpline.Create(HermiteSpline.Create(aNurbCurve.ControlPoints.Cast<oM.Geometry.Point>().ToList().ConvertAll(x => ToRevitPoint(x)), false));
+                return NurbSpline.Create(HermiteSpline.Create(aNurbCurve.ControlPoints.Cast<oM.Geometry.Point>().ToList().ConvertAll(x => ToRevit(x)), false));
             }
 
             if (ICurve is oM.Geometry.Ellipse)
             {
                 oM.Geometry.Ellipse aEllipse = ICurve as oM.Geometry.Ellipse;
-                return Autodesk.Revit.DB.Ellipse.CreateCurve(ToRevitPoint(aEllipse.Centre), aEllipse.Radius1, aEllipse.Radius2, ToRevitVector(aEllipse.Axis1), ToRevitVector(aEllipse.Axis2), 0, 1);
+                return Autodesk.Revit.DB.Ellipse.CreateCurve(ToRevit(aEllipse.Centre), aEllipse.Radius1, aEllipse.Radius2, ToRevit(aEllipse.Axis1), ToRevit(aEllipse.Axis2), 0, 1);
             }
 
             return null;
         }
 
-        public static WallType ToRevitWallType(this BuildingElementProperties BuildingElementProperties, Document Document)
+        public static CurveArray ToRevit(this PolyCurve PolyCurve)
         {
-            if (BuildingElementProperties.BuildingElementType != BuidingElementType.Wall)
+            if (PolyCurve == null)
                 return null;
 
-            WallType aWallType = new FilteredElementCollector(Document).OfClass(typeof(WallType)).First() as WallType;
-            if (aWallType == null)
-                return null;
+            CurveArray aCurveArray = new CurveArray();
+            foreach (ICurve aICurve in PolyCurve.Curves)
+                aCurveArray.Append(aICurve.ToRevit());
 
-            aWallType = aWallType.Duplicate(BuildingElementProperties.Name) as WallType;
-
-            return aWallType;
-
+            return aCurveArray;
         }
 
-        public static Wall ToRevitWall(this BuildingElement BuildingElement, Document Document)
+        public static ElementType ToRevit(this BuildingElementProperties BuildingElementProperties, Document Document)
         {
-            if(BuildingElement.BuildingElementGeometry is BuildingElementCurve)
+            if (BuildingElementProperties == null || Document == null)
+                return null;
+
+            ElementType aElementType = null;
+            switch (BuildingElementProperties.BuildingElementType)
             {
-                ICurve aICurve = (BuildingElement.BuildingElementGeometry as BuildingElementCurve).ICurve;
-
-                Storey aStorey = BuildingElement.Storey;
-                if (aStorey == null)
-                    return null;
-
-                List<Level> aLevelList = new FilteredElementCollector(Document).OfClass(typeof(Level)).Cast<Level>().ToList();
-
-                Level aLevel = aLevelList.Find(x => x.Elevation == aStorey.Elevation);
-                if (aLevel == null)
-                    aLevel = aStorey.ToRevitStorey(Document);
-
-                Wall aWall = Wall.Create(Document, ToRevitCurve(aICurve), aLevel.Id, false);
-
-                return aWall;
+                case BuidingElementType.Ceiling:
+                    aElementType = new FilteredElementCollector(Document).OfClass(typeof(CeilingType)).First() as ElementType;
+                    break;
+                case BuidingElementType.Floor:
+                    aElementType = new FilteredElementCollector(Document).OfClass(typeof(FloorType)).First() as ElementType;
+                    break;
+                case BuidingElementType.Roof:
+                    aElementType = new FilteredElementCollector(Document).OfClass(typeof(RoofType)).First() as ElementType;
+                    break;
+                case BuidingElementType.Wall:
+                    aElementType = new FilteredElementCollector(Document).OfClass(typeof(WallType)).First() as ElementType;
+                    break;
             }
 
-            return null;
+            aElementType = aElementType.Duplicate(BuildingElementProperties.Name) as WallType;
+
+            return aElementType;
         }
 
-        public static Level ToRevitStorey(this Storey Storey, Document Document)
+        public static Element ToRevit(this BuildingElement BuildingElement, Document Document)
+        {
+            if (BuildingElement == null || BuildingElement.BuildingElementProperties == null || Document == null)
+                return null;
+
+            Storey aStorey = BuildingElement.Storey;
+            if (aStorey == null)
+                return null;
+
+            List<Level> aLevelList = new FilteredElementCollector(Document).OfClass(typeof(Level)).Cast<Level>().ToList();
+
+            Level aLevel = aLevelList.Find(x => x.Elevation == aStorey.Elevation);
+            if (aLevel == null)
+                aLevel = aStorey.ToRevit(Document);
+
+            ICurve aICurve = BuildingElement.BuildingElementGeometry.Curve;
+
+            Element aElement = null;
+            switch (BuildingElement.BuildingElementProperties.BuildingElementType)
+            {
+                case BuidingElementType.Ceiling:
+                    break;
+                case BuidingElementType.Floor:
+                    break;
+                case BuidingElementType.Roof:
+                    break;
+                case BuidingElementType.Wall:
+                    aElement = Wall.Create(Document, ToRevit(aICurve), aLevel.Id, false);
+                    break;
+            }
+
+            return aElement;
+        }
+
+        public static Level ToRevit(this Storey Storey, Document Document)
         {
             Level aLevel = Level.Create(Document, Storey.Elevation);
             aLevel.Name = Storey.Name;
