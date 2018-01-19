63a5267318715b5bc7cde66765aa5c6a1720e62b
Jakub Ziolkowski <Jakub.Ziolkowski@BuroHappold.com>
2018-01-19 12:25:22 +0000

    CRUD Create and Read updated (BHoMObject to IObject)

--- Revit2018_Adapter/CRUD/Create.cs
+++ Revit2018_Adapter/CRUD/Create.cs
@@ -3,63 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 using BH.oM.Environmental.Elements;
-using BH.oM.Environmental.Properties;
 using BH.oM.Structural.Elements;
 using BH.Engine.Revit;
 using BH.Engine.Environment;
+using BH.oM.Base;
 
 using Autodesk.Revit.DB;
-
 namespace BH.Adapter.Revit
 {
-    public partial class RevitAdapter
+    public partial class RevitAdapter : BHoMAdapter
     {
-        public Building ReadBuilidng(Document Document, bool GenerateSpaces, bool Spaces3D)
+        protected override bool Create<T>(IEnumerable<T> objects, bool replaceAll = false)
         {
-            Building aBuilding = Document.FromRevit();
-
-            List<BuildingElementProperties> aBuildingElementPropertiesList = new FilteredElementCollector(Document).OfClass(typeof(WallType)).ToList().ConvertAll(x => ((WallType)x).FromRevit());
-
-            List<Level> aLevelList = new FilteredElementCollector(Document).OfClass(typeof(Level)).Cast<Level>().ToList();
-            List<Storey> aStoreyList = new List<Storey>();
-            if (aLevelList != null && aLevelList.Count > 0)
-                aStoreyList = aLevelList.ConvertAll(x => x.FromRevit());
-
-            List<SpatialElement> aSpatialElementList = new FilteredElementCollector(Document).OfCategory(BuiltInCategory.OST_MEPSpaces).Cast<SpatialElement>().ToList();
-            aSpatialElementList.RemoveAll(x => x.Area < 0.001);
-
-            aBuilding.Add(aStoreyList);
-
-            if (GenerateSpaces)
-            {
-                SpatialElementBoundaryOptions aSpatialElementBoundaryOptions = new SpatialElementBoundaryOptions();
-                aSpatialElementBoundaryOptions.SpatialElementBoundaryLocation = SpatialElementBoundaryLocation.Center;
-                aSpatialElementBoundaryOptions.StoreFreeBoundaryFaces = true;
-
-                if (Spaces3D)
-                {
-                    SpatialElementGeometryCalculator aSpatialElementGeometryCalculator = new SpatialElementGeometryCalculator(Document, aSpatialElementBoundaryOptions);
-                    foreach (SpatialElement aSpatialElement in aSpatialElementList)
-                    {
-                        Space aSpace = aSpatialElement.FromRevit(aSpatialElementGeometryCalculator, aBuildingElementPropertiesList, aStoreyList);
-                        if (aSpace != null)
-                            aBuilding.Add(aSpace);
-                    }
-                }
-                else
-                {
-                    foreach (SpatialElement aSpatialElement in aSpatialElementList)
-                    {
-                        Space aSpace = aSpatialElement.FromRevit(aSpatialElementBoundaryOptions, aBuildingElementPropertiesList, aStoreyList);
-                        if (aSpace != null)
-                            aBuilding.Add(aSpace);
-                    }
-                }
-            }
-
-            return aBuilding;
+            throw new NotImplementedException();
         }
     }
 }
