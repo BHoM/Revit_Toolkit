904d5386f5783cbb3a753b59b37b727b5929ec33
Jakub Ziolkowski <Jakub.Ziolkowski@BuroHappold.com>
2018-01-19 12:25:22 +0000

    CRUD Create and Read updated (BHoMObject to IObject)

--- Revit2018_Adapter/CRUD/Read.cs
+++ Revit2018_Adapter/CRUD/Read.cs
@@ -1,23 +1,77 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using BH.oM.Base;
+using BH.oM.Environmental.Elements;
+using BH.Engine.Revit;
+using BH.oM.Structural.Elements;
+using BH.Engine.Environment;
+
 using Autodesk.Revit.DB;
 
-using BH.oM.Base;
 
 namespace BH.Adapter.Revit
 {
-    public partial class RevitAdapter
+    public partial class RevitAdapter : BHoMAdapter
     {
-        public IEnumerable<BHoMObject> Read(Document Document, IEnumerable<BuiltInCategory> BuiltInCategories)
+        public Building ReadBuilidng(Document Document, bool GenerateSpaces, bool Spaces3D)
         {
-            throw new NotImplementedException();
+            Building aBuilding = Document.FromRevit();
+
+            //Adding BuilidngElementProperties
+            aBuilding.BuildingElementProperties = new FilteredElementCollector(Document).OfClass(typeof(WallType)).ToList().ConvertAll(x => ((WallType)x).FromRevit());
+
+            //Adding Storeys
+            List<Level> aLevelList = new FilteredElementCollector(Document).OfClass(typeof(Level)).Cast<Level>().ToList();
+            List<Storey> aStoreyList = new List<Storey>();
+            if (aLevelList != null && aLevelList.Count > 0)
+                aStoreyList = aLevelList.ConvertAll(x => x.FromRevit());
+            aBuilding.Add(aStoreyList);
+
+
+            //Adding Spaces
+            List<SpatialElement> aSpatialElementList = new FilteredElementCollector(Document).OfCategory(BuiltInCategory.OST_MEPSpaces).Cast<SpatialElement>().ToList();
+            aSpatialElementList.RemoveAll(x => x.Area < 0.001);
+
+
+            //Adding Spaces
+            if (GenerateSpaces)
+            {
+                SpatialElementBoundaryOptions aSpatialElementBoundaryOptions = new SpatialElementBoundaryOptions();
+                aSpatialElementBoundaryOptions.SpatialElementBoundaryLocation = SpatialElementBoundaryLocation.Center;
+                aSpatialElementBoundaryOptions.StoreFreeBoundaryFaces = true;
+
+                if (Spaces3D)
+                {
+                    //3D geometry
+                    SpatialElementGeometryCalculator aSpatialElementGeometryCalculator = new SpatialElementGeometryCalculator(Document, aSpatialElementBoundaryOptions);
+                    foreach (SpatialElement aSpatialElement in aSpatialElementList)
+                    {
+                        Space aSpace = aSpatialElement.FromRevit(aSpatialElementGeometryCalculator, aBuilding.BuildingElementProperties, aStoreyList);
+                        if (aSpace != null)
+                            aBuilding.Add(aSpace);
+                    }
+                }
+                else
+                {
+                    //2D geometry
+                    foreach (SpatialElement aSpatialElement in aSpatialElementList)
+                    {
+                        Space aSpace = aSpatialElement.FromRevit(aSpatialElementBoundaryOptions, aBuilding.BuildingElementProperties, aStoreyList);
+                        if (aSpace != null)
+                            aBuilding.Add(aSpace);
+                    }
+                }
+            }
+
+            return aBuilding;
         }
 
-        public IEnumerable<BHoMObject> Read(Document Document, IEnumerable<string> UniqueIds)
+        protected override IEnumerable<IObject> Read(Type type, IList ids)
         {
             throw new NotImplementedException();
         }
