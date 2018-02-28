7364ef9037d0e15ea6bd7cc23951d816389bf2e0
INaslund <isak.naslund@burohappold.com>
2018-02-28 12:14:13 +0100

    Adding revit external adapter project

---
+++ Revit2018External_Adapter/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revit2018External_Adapter
+{
+    public class Class1
+    {
+    }
+}
