16f6ff1ec4d101ff78c8e8839843c1f1c8410643
INaslund <isak.naslund@burohappold.com>
2018-03-01 16:03:55 +0100

    SImple update ports form added

---
+++ RevitListener/UpdatePorts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using BH.Adapter.Revit.Forms;
+
+namespace BH.Adapter.Revit
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class UpdatePorts : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            new UpdatePortsForm().ShowDialog();
+
+            return Result.Succeeded;
+        }
+
+    }
+}
