3efc5758a4da4d733ccb00c9d2b946b7792bda51
Jakub Ziolkowski <Jakub.Ziolkowski@BuroHappold.com>
2018-03-02 14:15:19 +0000

    Discipline added for ToBHoM methods

---
+++ Revit2018_Engine/Convert/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Revit
+{
+    public enum Discipline
+    {
+        Environmental,
+        Structural
+    }
+}
