93dfbf6424481beb8b244cbad89dfa568e350a71
Jakub Ziolkowski <Jakub.Ziolkowski@BuroHappold.com>
2018-03-06 13:24:18 +0000

    Get top of Revit Solid moved to Query

---
+++ Revit2018_Engine/Query/Top.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+
+namespace BH.Engine.Revit
+{
+    /// <summary>
+    /// BHoM Revit Engine Modify Methods
+    /// </summary>
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        /// <summary>
+        /// Gets Top Planar Face of Revit Solid
+        /// </summary>
+        /// <param name="solid">Revit Solid</param>
+        /// <returns name="PlanarFace">Revit PlanarFace</returns>
+        /// <search>
+        /// Query, Planar Face, Solid, Top, planarface
+        /// </search>
+        static public PlanarFace Top(this Solid solid)
+        {
+            PlanarFace aResult = null;
+            FaceArray aFaceArray = solid.Faces;
+            foreach (Face aFace in aFaceArray)
+            {
+                PlanarFace aPlanarFace = aFace as PlanarFace;
+                if (null != aPlanarFace && Query.IsHorizontal(aPlanarFace))
+                    if ((null == aResult) || (aResult.Origin.Z < aPlanarFace.Origin.Z))
+                        aResult = aPlanarFace;
+            }
+            return aResult;
+        }
+
+        /***************************************************/
+    }
+}
