fdf045cbc2fc81503d9a934ab7d938648e98e173
Jakub Ziolkowski <Jakub.Ziolkowski@BuroHappold.com>
2018-03-20 15:22:56 +0000

    Space Adjacency update (work in progress)

---
+++ Revit2018_Engine/Query/BuildingElements.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+using BH.oM.Environmental.Elements;
+
+using Autodesk.Revit.DB;
+
+
+namespace BH.Engine.Revit
+{
+    /// <summary>
+    /// BHoM Revit Engine Query Methods
+    /// </summary>
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        /// <summary>
+        /// Get BuilidingElements by given Revit ElementId
+        /// </summary>
+        /// <param name="building">BHoM Building</param>
+        /// <param name="elementId">Revit ElementId</param>
+        /// <returns name="BuildingElements">BHoM BuildingElements</returns>
+        /// <search>
+        /// Query, BuildingElements, ElementId
+        /// </search>
+        public static List<BuildingElement> BuildingElements(this Building building, ElementId elementId)
+        {
+            if (elementId == null || elementId == Autodesk.Revit.DB.ElementId.InvalidElementId)
+                return null;
+
+            return BuildingElements(building, elementId.IntegerValue);
+        }
+
+        /// <summary>
+        /// Get BuilidingElements by given integer value of Revit ElementId
+        /// </summary>
+        /// <param name="building">BHoM Building</param>
+        /// <param name="elementId">Integer value of Revit ElementId</param>
+        /// <returns name="BuildingElements">BHoM BuildingElements</returns>
+        /// <search>
+        /// Query, BuildingElements, ElementId
+        /// </search>
+        public static List<BuildingElement> BuildingElements(this Building building, int elementId)
+        {
+            if (building == null)
+                return null;
+
+            List<BuildingElement> aResult = new List<BuildingElement>();
+            foreach (BuildingElement aBuildingElement in building.BuildingElements)
+            {
+                object aValue;
+                if (aBuildingElement.CustomData.TryGetValue(Convert.ElementId, out aValue))
+                    if (aValue is int && (int)aValue == elementId)
+                        aResult.Add(aBuildingElement);
+            }
+            return aResult;
+        }
+
+        /***************************************************/
+    }
+}
+
---
+++ Revit2018_Engine/Query/Element.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Base;
+
+using Autodesk.Revit.DB;
+
+namespace BH.Engine.Revit
+{
+    /// <summary>
+    /// BHoM Revit Engine Query Methods
+    /// </summary>
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        /// <summary>
+        /// Gets Revit ELement from Unique Id and LinkUniqueId
+        /// </summary>
+        /// <param name="document">Revit Document</param>
+        /// <param name="uniqueId">Revit UniqueId</param>
+        /// <param name="linkUniqueId">Revit Link UniqueId</param>
+        /// <returns name="Element">Revit Element</returns>
+        /// <search>
+        /// Query, BHoM, Element, linkUniqueId, uniqueId
+        /// </search>
+        public static Element Element(this Document document, string uniqueId, string linkUniqueId = null)
+        {
+            if (document == null)
+                return null;
+
+            Document aDocument = null;
+
+            if (!string.IsNullOrEmpty(linkUniqueId))
+            {
+                RevitLinkInstance aRevitLinkInstance = document.GetElement(linkUniqueId) as RevitLinkInstance;
+                if (aRevitLinkInstance != null)
+                    aDocument = aRevitLinkInstance.GetLinkDocument();
+            }
+            else
+            {
+                aDocument = document;
+            }
+
+            if (aDocument == null)
+                return null;
+
+            return aDocument.GetElement(uniqueId);
+        }
+
+        /***************************************************/
+
+        /// <summary>
+        /// Gets Revit Element from LinkElementId
+        /// </summary>
+        /// <param name="document">Revit Document</param>
+        /// <param name="linkElementId">Revit LinkElementId</param>
+        /// <returns name="Element">Revit Element</returns>
+        /// <search>
+        /// Query, BHoM, Element, LinkElementId
+        /// </search>
+        public static Element Element(this Document document, LinkElementId linkElementId)
+        {
+            if (document == null || linkElementId == null)
+                return null;
+
+            Document aDocument = null;
+            if (linkElementId.LinkInstanceId != Autodesk.Revit.DB.ElementId.InvalidElementId)
+                aDocument = (document.GetElement(linkElementId.LinkInstanceId) as RevitLinkInstance).GetLinkDocument();
+            else
+                aDocument = document;
+
+            if (linkElementId.LinkedElementId != Autodesk.Revit.DB.ElementId.InvalidElementId)
+                return aDocument.GetElement(linkElementId.LinkedElementId);
+            else
+                return aDocument.GetElement(linkElementId.HostElementId);
+        }
+
+        /***************************************************/
+    }
+}
+
