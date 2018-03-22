dfa29814c1c65bb5fea2f0a1a387222a22be9d7b
Jakub Ziolkowski <Jakub.Ziolkowski@BuroHappold.com>
2018-03-22 18:21:06 +0000

    ICustomData method added

---
+++ Revit2018_Engine/Query/CustomData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Base;
+
+namespace BH.Engine.Revit
+{
+    /// <summary>
+    /// BHoM Revit Engine Query Methods
+    /// </summary>
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        /// <summary>
+        /// Returs CustomData value
+        /// </summary>
+        /// <param name="bHoMObject">BHoMObject</param>
+        /// <param name="name">BHoMObject CustomData Name</param>
+        /// <returns name="ElementId">Revit ElementId</returns>
+        /// <search>
+        /// Query, BHoM, ICustomData
+        /// </search>
+        public static object ICustomData(this IBHoMObject bHoMObject, string name)
+        {
+            if (bHoMObject == null)
+                return null;
+
+            object aObject;
+            if (bHoMObject.CustomData.TryGetValue(name, out aObject))
+                return aObject;
+
+            return null;
+        }
+
+        /***************************************************/
+    }
+}
+
