6a9ef626d19e52b0bb751d36ebd1d7858824a6f1
Pawel Baran <pawel.baran@burohappold.com>
2018-08-01 18:37:01 +0200

    Method for checking an object for null properties

--- Revit_Engine/Convert/Errors.cs
+++ Revit_Engine/Convert/Errors.cs
@@ -2,6 +2,8 @@
 using Autodesk.Revit.DB.Structure;
 using BH.oM.Base;
 using System.Collections.Generic;
+using System.Reflection;
+using System;
 
 namespace BH.Engine.Revit
 {
@@ -48,6 +50,36 @@ namespace BH.Engine.Revit
         {
             if (document == null)
                 Engine.Reflection.Compute.RecordError(string.Format("BHoM object could not be read because Revit document with title {0} does not exist.", document.Title));
+        }
+
+        /***************************************************/
+
+        private static void CheckNullProperties(this BHoMObject obj, IEnumerable<string> propertyNames = null)
+        {
+            List<string> nullPropertyNames = new List<string>();
+
+            Type type = obj.GetType();
+            if (propertyNames == null)
+            {
+                PropertyInfo[] properties = type.GetProperties();
+                foreach (PropertyInfo pi in properties)
+                {
+                    if (pi.GetValue(obj) == null) nullPropertyNames.Add(pi.Name);
+                }
+            }
+            else
+            {
+                foreach (string propertyName in propertyNames)
+                {
+                    if (type.GetProperty(propertyName).GetValue(obj) == null) nullPropertyNames.Add(propertyName);
+                }
+            }
+            
+            string warning = string.Format("The BHoM object if missing following properties: {0}. BHoM_Guid: {1}.", string.Join(", ", nullPropertyNames), obj.BHoM_Guid);
+
+            ElementId revitId = obj.ElementId();
+            if (revitId != null) warning += string.Format(" Revit ElementId: {0}.", revitId);
+            Engine.Reflection.Compute.RecordWarning(warning);
         }
 
         /***************************************************/
