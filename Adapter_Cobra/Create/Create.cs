9dd67ed23a0885ec7d1d298cb30f5d1cfe2d354c
Jakub Ziolkowski <Jakub.Ziolkowski@burohappold.com>
2018-08-02 16:08:05 +0100

    Preparing Cobra Adapter for changes (work in progress)

---
+++ Adapter_Cobra/Create/Create.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using BH.oM.Environment.Elements;
+using BH.oM.Environment.Properties;
+using BH.oM.Structural.Elements;
+using BH.Engine.Revit;
+using BH.oM.Base;
+
+using Autodesk.Revit.DB;
+
+
+namespace BH.UI.Revit.Adapter
+{
+    public partial class CobraAdapter : BH.Adapter.Revit.InternalRevitAdapter
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+
+        /***************************************************/
+        /**** Protected Methods                         ****/
+        /***************************************************/
+
+        /// <summary>
+        /// Create BHoMObjects in Revit Document. BHoMObjects are linked to Revit elements by CustomData parameter called by Utilis.AdapterId const. Use Utilis.BHoM.CopyIdentifiers to include necessary information in BHoMObject
+        /// </summary>
+        /// <param name="objects">BHoMObjects collection</param>
+        /// <param name="replaceAll">Replace exisiting Revit Elements. Existing elements will be matched by CustomData parameter called by Utilis.AdapterId const.</param>
+        /// <returns name="Succeeded">Create succeeded</returns>
+        /// <search>
+        /// Create, BHoMObjects, BHoMObject, Revit, Document
+        /// </search>
+        protected override bool Create<T>(IEnumerable<T> objects, bool replaceAll = false)
+        {
+            if (Document == null)
+            {
+                NullDocumentCreateError();
+                return false;
+            }
+
+            if (objects == null)
+            {
+                NullObjectsCreateError();
+                return false;
+            }
+
+            if (objects.Count() < 1)
+                return false;
+
+            using (Transaction aTransaction = new Transaction(Document, "Create"))
+            {
+                aTransaction.Start();
+
+                foreach (IBHoMObject aBHOMObject in objects)
+                {
+                    if(aBHOMObject == null)
+                    {
+                        NullObjectCreateError(typeof(IBHoMObject));
+                        continue;
+                    }
+
+                    try
+                    {
+                        if (aBHOMObject is oM.Architecture.Elements.Level || 
+                            aBHOMObject is BuildingElement || 
+                            aBHOMObject is BuildingElementProperties)
+                        {
+                            Create(aBHOMObject as dynamic, true, replaceAll);
+                        }  
+                        else
+                        {
+                            if (replaceAll)
+                                Delete(aBHOMObject as BHoMObject);
+
+                            Engine.Revit.Convert.ToRevit(aBHOMObject as dynamic, Document, true);
+                        }
+                            
+                    }
+                    catch(Exception aException)
+                    {
+                        ObjectNotCreatedCreateError(aBHOMObject);
+                    }
+
+                }
+                    
+                aTransaction.Commit();
+            }
+
+            return true;
+        }
+
+        /***************************************************/
+        /**** Private Methods                           ****/
+        /***************************************************/
+
+        private bool Create(FramingElement framingElement, bool copyCustomData = true, bool replace = false)
+        {
+            if (framingElement == null)
+            {
+                Engine.Reflection.Compute.RecordError("Revit Object could not be created because BHoM FramingElement is null.");
+                return false;
+            }
+
+            FamilyInstance aFamilyInstance = framingElement.ToRevitColumn(Document, copyCustomData);
+
+            return true;
+        }
+
+        /***************************************************/
+    }
+}
---
+++ Adapter_Cobra/Create/Environment/BuildingElementProperties.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using BH.oM.Environment.Elements;
+using BH.oM.Environment.Properties;
+using BH.oM.Structural.Elements;
+using BH.Engine.Revit;
+using BH.oM.Base;
+
+using Autodesk.Revit.DB;
+
+
+namespace BH.UI.Revit.Adapter
+{
+    public partial class CobraAdapter
+    {
+        /***************************************************/
+        /**** Private Methods                           ****/
+        /***************************************************/
+
+        private bool Create(BuildingElementProperties buildingElementProperties, bool copyCustomData = true, bool replace = false)
+        {
+            if (buildingElementProperties == null)
+            {
+                NullObjectCreateError(typeof(BuildingElementProperties));
+                return false;
+            }
+
+            if (replace)
+                Delete(buildingElementProperties);
+
+            Type aType = Query.RevitType(buildingElementProperties.BuildingElementType);
+            List<Element> aElementList = new FilteredElementCollector(Document).OfClass(aType).ToList();
+            if (aElementList == null && aElementList.Count < 1)
+                return false;
+
+            Element aElement = aElementList.Find(x => x.Name == buildingElementProperties.Name);
+            if (aElement == null)
+            {
+                aElement = buildingElementProperties.ToRevit(Document, copyCustomData);
+                return true;
+            }
+
+            return false;
+        }
+
+        /***************************************************/
+    }
+}
