68eac98beefdb0325bd93e6b1730337995ab4e57
Jakub Ziolkowski <Jakub.Ziolkowski@burohappold.com>
2018-08-03 11:24:05 +0100

    Minor updates for PR

--- Adapter_Cobra/Update/UpdateProperty.cs
+++ Adapter_Cobra/Update/UpdateProperty.cs
@@ -12,39 +12,37 @@ namespace BH.UI.Revit.Adapter
         /**** Public Methods                            ****/
         /***************************************************/
 
-        public override int UpdateProperty(Type type, IEnumerable<object> ids, string property, object newValue)
-        {
+        //public override int UpdateProperty(Type type, IEnumerable<object> ids, string property, object newValue)
+        //{
 
-            if (property == "Tags")
-            {
-                List<int> indecies = ids.Select(x => (int)x).ToList();
-                if (indecies.Count < 1)
-                    return 0;
+        //    if (property == "Tags")
+        //    {
+        //        List<int> indecies = ids.Select(x => (int)x).ToList();
+        //        if (indecies.Count < 1)
+        //            return 0;
 
-                List<HashSet<string>> tags = (newValue as IEnumerable<HashSet<string>>).ToList();
-                return UpdateTags(type, indecies, tags);
-            }
+        //        List<HashSet<string>> tags = (newValue as IEnumerable<HashSet<string>>).ToList();
+        //        return UpdateTags(type, indecies, tags);
+        //    }
 
-            return 0;
-        }
+        //    return 0;
+        //}
 
-        /***************************************************/
-        /**** Private Methods                           ****/
-        /***************************************************/
+        ///***************************************************/
+        ///**** Private Methods                           ****/
+        ///***************************************************/
 
-        private int UpdateTags(Type t, List<int> indecies, List<HashSet<string>> tags)
-        {
-            //Dictionary<int, HashSet<string>> typeTags = this.GetTypeTags(t);// = m_tags[t];
+        //private int UpdateTags(Type t, List<int> indecies, List<HashSet<string>> tags)
+        //{
+        //    Dictionary<int, HashSet<string>> typeTags = this.GetTypeTags(t);// = m_tags[t];
 
-            //for (int i = 0; i < indecies.Count; i++)
-            //{
-            //    typeTags[indecies[i]] = tags[i];
-            //}
+        //    for (int i = 0; i < indecies.Count; i++)
+        //    {
+        //        typeTags[indecies[i]] = tags[i];
+        //    }
 
-            //return indecies.Count;
-
-            return 0;
-        }
+        //    return indecies.Count;
+        //}
 
         /***************************************************/
     }
