477c30802a261c085b728ee17ddffd58acfc35cb
Pawel Baran <pawel.baran@burohappold.com>
2018-08-08 19:48:50 +0200

    Added property collectors to Wall, Floor & FamilyInstance

--- Cobra_Engine/Convert/Structure/ToBHoM/PanelPlanar.cs
+++ Cobra_Engine/Convert/Structure/ToBHoM/PanelPlanar.cs
@@ -3,6 +3,8 @@ using BH.oM.Structural.Elements;
 using BH.oM.Structural.Properties;
 using System.Collections.Generic;
 using BHS = BH.Engine.Structure;
+using BH.oM.Base;
+using System.Linq;
 
 namespace BH.Engine.Revit
 {
@@ -12,11 +14,24 @@ namespace BH.Engine.Revit
         /****             Internal methods              ****/
         /***************************************************/
 
-        internal static List<PanelPlanar> ToBHoMPanelPlanar(this Wall wall, bool copyCustomData = true, bool convertUnits = true)
+        internal static List<PanelPlanar> ToBHoMPanelPlanar(this Wall wall, Dictionary<ElementId, List<IBHoMObject>> objects = null, bool copyCustomData = true, bool convertUnits = true)
         {
-            string materialGrade = wall.MaterialGrade();
-            IProperty2D aProperty2D = wall.WallType.ToBHoMProperty2D(materialGrade, copyCustomData, convertUnits) as IProperty2D;
+            IProperty2D aProperty2D = null;
+            if (objects != null)
+            {
+                List<IBHoMObject> aBHoMObjectList = new List<IBHoMObject>();
+                if (objects.TryGetValue(wall.WallType.Id, out aBHoMObjectList))
+                    if (aBHoMObjectList != null && aBHoMObjectList.Count > 0)
+                        aProperty2D = aBHoMObjectList.First() as IProperty2D;
+            }
 
+            if (aProperty2D == null)
+            {
+                aProperty2D = wall.WallType.ToBHoMProperty2D(objects, copyCustomData, convertUnits) as IProperty2D;
+                if (objects != null)
+                    objects.Add(wall.WallType.Id, new List<IBHoMObject>(new IBHoMObject[] { aProperty2D }));
+            }
+            
             List<oM.Geometry.ICurve> outlines = wall.Outlines();
             List<PanelPlanar> aResult = outlines != null ? BHS.Create.PanelPlanar(outlines) : new List<PanelPlanar> { new PanelPlanar { ExternalEdges = new List<oM.Structural.Elements.Edge>(), Openings = new List<oM.Structural.Elements.Opening>(), Property = aProperty2D } };
 
@@ -27,9 +42,7 @@ namespace BH.Engine.Revit
 
                 panel = Modify.SetIdentifiers(panel, wall) as PanelPlanar;
                 if (copyCustomData)
-                {
                     panel = Modify.SetCustomData(panel, wall, convertUnits) as PanelPlanar;
-                }
             }
 
             return aResult;
@@ -37,10 +50,23 @@ namespace BH.Engine.Revit
 
         /***************************************************/
 
-        internal static List<PanelPlanar> ToBHoMPanelPlanar(this Floor floor, bool copyCustomData = true, bool convertUnits = true)
+        internal static List<PanelPlanar> ToBHoMPanelPlanar(this Floor floor, Dictionary<ElementId, List<IBHoMObject>> objects = null, bool copyCustomData = true, bool convertUnits = true)
         {
-            string materialGrade = floor.MaterialGrade();
-            IProperty2D aProperty2D = floor.FloorType.ToBHoMProperty2D(materialGrade, copyCustomData, convertUnits) as IProperty2D;
+            IProperty2D aProperty2D = null;
+            if (objects != null)
+            {
+                List<IBHoMObject> aBHoMObjectList = new List<IBHoMObject>();
+                if (objects.TryGetValue(floor.FloorType.Id, out aBHoMObjectList))
+                    if (aBHoMObjectList != null && aBHoMObjectList.Count > 0)
+                        aProperty2D = aBHoMObjectList.First() as IProperty2D;
+            }
+
+            if (aProperty2D == null)
+            {
+                aProperty2D = floor.FloorType.ToBHoMProperty2D(objects, copyCustomData, convertUnits) as IProperty2D;
+                if (objects != null)
+                    objects.Add(floor.FloorType.Id, new List<IBHoMObject>(new IBHoMObject[] { aProperty2D }));
+            }
 
             List<oM.Geometry.ICurve> outlines = floor.Outlines();
             List<PanelPlanar> aResult = outlines != null ? BHS.Create.PanelPlanar(outlines) : new List<PanelPlanar> { new PanelPlanar { ExternalEdges = new List<oM.Structural.Elements.Edge>(), Openings = new List<oM.Structural.Elements.Opening>(), Property = aProperty2D } };
@@ -52,9 +78,7 @@ namespace BH.Engine.Revit
 
                 panel = Modify.SetIdentifiers(panel, floor) as PanelPlanar;
                 if (copyCustomData)
-                {
                     panel = Modify.SetCustomData(panel, floor, convertUnits) as PanelPlanar;
-                }
             }
 
             return aResult;
