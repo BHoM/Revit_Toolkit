a3f05c7d40b1f7a110badcfe1232a8303de18bd8
Isak Naslund <Isak.Naslund@burohappold.com>
2018-08-13 16:43:15 +0200

    Adding custom Push for revit going straight to create

---
+++ Adapter_Cobra/Push/Push.cs
@@ -0,0 +1,48 @@
+﻿using Autodesk.Revit.DB;
+using BH.oM.Base;
+using BH.oM.Revit;
+using BH.UI.Cobra.Engine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace BH.UI.Cobra.Adapter
+{
+    public partial class CobraAdapter
+    {
+
+        /***************************************************/
+        /**** Protected Methods                         ****/
+        /***************************************************/
+
+        public override List<IObject> Push(IEnumerable<IObject> objects, string tag = "", Dictionary<string, object> config = null)
+        {
+
+            bool success = true;
+
+            List<IObject> objectsToPush = Config.CloneBeforePush ? objects.Select(x => x is BHoMObject ? ((BHoMObject)x).GetShallowClone() : x).ToList() : objects.ToList(); //ToList() necessary for the return collection to function properly for cloned objects
+
+            Type iBHoMObjectType = typeof(IBHoMObject);
+            MethodInfo miToList = typeof(Enumerable).GetMethod("Cast");
+            foreach (var typeGroup in objectsToPush.GroupBy(x => x.GetType()))
+            {
+                MethodInfo miListObject = miToList.MakeGenericMethod(new[] { typeGroup.Key });
+
+                var list = miListObject.Invoke(typeGroup, new object[] { typeGroup });
+
+                if (iBHoMObjectType.IsAssignableFrom(typeGroup.Key))
+                {
+                   success &= Create(list as dynamic);
+
+                }
+            }
+
+            return success ? objectsToPush : new List<IObject>();
+
+
+        }
+
+    }
+}
