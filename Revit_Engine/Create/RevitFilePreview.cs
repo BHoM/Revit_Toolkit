3afbf786b4046e7783854310f7cbd9c6054b4b77
Jakub Ziolkowski <Jakub.Ziolkowski@burohappold.com>
2018-09-03 12:14:35 +0100

    RevitFilePreview added

---
+++ Revit_Engine/Create/RevitFilePreview.cs
@@ -0,0 +1,90 @@
+﻿using BH.oM.Revit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Packaging;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Xml.Linq;
+
+namespace BH.Engine.Revit
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static RevitFilePreview RevitFilePreview(string Path)
+        {
+            XDocument aXDocument = null;
+
+            if (!File.Exists(Path))
+                return null;
+
+            StorageInfo aStorageInfo = (StorageInfo)InvokeStorageRootMethod(null, "Open", Path, FileMode.Open, FileAccess.Read, FileShare.Read);
+
+            if (aStorageInfo != null)
+            {
+                StreamInfo[] aStreamInfoArray = aStorageInfo.GetStreams();
+                List<string> aNames = aStreamInfoArray.ToList().ConvertAll(x => x.Name);
+                foreach (StreamInfo aStreamInfo in aStreamInfoArray)
+                {
+                    if (aStreamInfo.Name.Equals("PartAtom"))
+                    {
+                        byte[] aFileInfoData = ParseStreamInfo(aStreamInfo);
+                        aXDocument = XDocument.Parse(Encoding.Default.GetString(aFileInfoData));
+                    }
+                }
+
+                CloseStorageInfo(aStorageInfo);
+            }
+
+            RevitFilePreview aRevitFilePreview = new RevitFilePreview()
+            {
+                XDocument = aXDocument
+            };
+
+            return aRevitFilePreview;
+        }
+
+        /***************************************************/
+
+        private static object InvokeStorageRootMethod(StorageInfo StorageInfoRoot, string MethodName, params object[] MethodArgs)
+        {
+            BindingFlags aBindingFlags = BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.InvokeMethod;
+            Type aStorageRootType = typeof(StorageInfo).Assembly.GetType("System.IO.Packaging.StorageRoot", true, false);
+            object aResult = aStorageRootType.InvokeMember(MethodName, aBindingFlags, null, StorageInfoRoot, MethodArgs);
+            return aResult;
+        }
+
+        private static byte[] ParseStreamInfo(StreamInfo StreamInfo)
+        {
+            byte[] aResult = null;
+            try
+            {
+                using (Stream aStream = StreamInfo.GetStream(FileMode.Open, FileAccess.Read))
+                {
+                    aResult = new byte[aStream.Length];
+                    aStream.Read(aResult, 0, aResult.Length);
+                    return aResult;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                aResult = null;
+            }
+        }
+
+        private static void CloseStorageInfo(StorageInfo StorageInfo)
+        {
+            InvokeStorageRootMethod(StorageInfo, "Close");
+        }
+    }
+}
+
---
+++ Revit_Engine/Query/FamilyTypeNames.cs
@@ -0,0 +1,56 @@
+﻿using BH.oM.Revit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace BH.Engine.Revit
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static List<string> FamilyTypeNames(this RevitFilePreview RevitFilePreview)
+        {
+            if (RevitFilePreview == null || RevitFilePreview.XDocument == null)
+                return null;
+
+            XDocument aXDocument = RevitFilePreview.XDocument;
+
+            if (aXDocument != null && aXDocument.Root != null && aXDocument.Root.Attributes() != null)
+            {
+                List<XAttribute> aAttributeList = aXDocument.Root.Attributes().ToList();
+                XAttribute aFirstAttribute = aAttributeList.Find(x => x.Name.LocalName == "A");
+                XAttribute aSecondAttribute = aAttributeList.Find(x => x.Name.LocalName == "xmlns");
+
+                if (aFirstAttribute != null && aSecondAttribute != null)
+                {
+                    XName aName = XName.Get("family", aFirstAttribute.Value);
+                    XElement aXElement = aXDocument.Root.Element(aName);
+                    if (aXElement != null)
+                    {
+                        aName = XName.Get("part", aFirstAttribute.Value);
+                        List<XElement> aXElementList = aXElement.Elements(aName).ToList();
+                        if (aXElementList != null)
+                        {
+                            List<string> aResult = new List<string>();
+                            aName = XName.Get("title", aSecondAttribute.Value);
+                            foreach (XElement XElement_Type in aXElementList)
+                            {
+                                XElement aXElement_Title = XElement_Type.Element(aName);
+                                if (aXElement_Title != null && !string.IsNullOrEmpty(aXElement_Title.Value))
+                                    aResult.Add(aXElement_Title.Value);
+                            }
+                            return aResult;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        /***************************************************/
+    }
+}
