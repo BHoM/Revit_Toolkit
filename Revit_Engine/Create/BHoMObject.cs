67db9016b478a612c3911685967cf0942e55c5e3
Jakub Ziolkowski <Jakub.Ziolkowski@burohappold.com>
2018-09-11 12:11:22 +0100

    Adding constructors for Generic BHoM objects

---
+++ Revit_Engine/Create/BHoMObject.cs
@@ -0,0 +1,40 @@
+﻿using BH.oM.Base;
+
+namespace BH.Engine.Adapters.Revit
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static BHoMObject BHoMObject(int ElementId)
+        {
+            BHoMObject aBHoMObject = new BHoMObject()
+            {
+
+            };
+
+            aBHoMObject.CustomData.Add(Convert.ElementId, ElementId);
+
+            return aBHoMObject;
+        }
+
+        /***************************************************/
+
+        public static BHoMObject BHoMObject(string UniqueId)
+        {
+            BHoMObject aBHoMObject = new BHoMObject()
+            {
+
+            };
+
+            aBHoMObject.CustomData.Add(Convert.AdapterId, UniqueId);
+
+            return aBHoMObject;
+        }
+
+        /***************************************************/
+    }
+}
+
