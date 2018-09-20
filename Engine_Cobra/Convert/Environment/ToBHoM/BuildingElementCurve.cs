733697b7935eb513a2549a39e9d91d51c09e9756
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-09-20 12:23:11 +0100

    Updating BuildingElement creation - Issue #55

--- Engine_Cobra/Convert/Environment/ToBHoM/BuildingElementCurve.cs
+++ Engine_Cobra/Convert/Environment/ToBHoM/BuildingElementCurve.cs
@@ -4,6 +4,9 @@ using BH.oM.Environment.Elements;
 using BH.oM.Adapters.Revit.Settings;
 using BH.oM.Geometry;
 
+using System;
+using System.Collections.Generic;
+
 namespace BH.UI.Cobra.Engine
 {
     public static partial class Convert
@@ -12,11 +15,43 @@ namespace BH.UI.Cobra.Engine
         /****             Internal methods              ****/
         /***************************************************/
 
-        internal static ICurve ToBHoMBuildingElementCurve(this Wall wall, PullSettings pullSettings = null)
+        internal static ICurve ToBHoMCurve(this Wall wall, PullSettings pullSettings = null)
         {
             pullSettings = pullSettings.DefaultIfNull();
 
             return (wall.Location as LocationCurve).ToBHoM(pullSettings);
+        }
+
+        internal static ICurve ToBHoMCurve(this FamilyInstance familyInstance, PullSettings pullSettings = null)
+        {
+            pullSettings = pullSettings.DefaultIfNull();
+
+            //TODO: Get more accurate shape. Currently, Windows and doors goes as rectangular panel
+            BoundingBoxXYZ aBoundingBoxXYZ = familyInstance.get_BoundingBox(null);
+
+            XYZ aVector = aBoundingBoxXYZ.Max - aBoundingBoxXYZ.Min;
+
+            double aWidth = Math.Abs(aVector.Y);
+            double aHeight = Math.Abs(aVector.Z);
+
+            XYZ aVector_Y = (aBoundingBoxXYZ.Transform.BasisY * aWidth) / 2;
+            XYZ aVector_Z = (aBoundingBoxXYZ.Transform.BasisZ * aHeight) / 2;
+
+            XYZ aMiddle = (aBoundingBoxXYZ.Max + aBoundingBoxXYZ.Min) / 2;
+
+            XYZ aXYZ_1 = aMiddle + aVector_Z - aVector_Y;
+            XYZ aXYZ_2 = aMiddle + aVector_Z + aVector_Y;
+            XYZ aXYZ_3 = aMiddle - aVector_Z + aVector_Y;
+            XYZ aXYZ_4 = aMiddle - aVector_Z - aVector_Y;
+
+            List<oM.Geometry.Point> aPointList = new List<oM.Geometry.Point>();
+            aPointList.Add(aXYZ_1.ToBHoM(pullSettings));
+            aPointList.Add(aXYZ_2.ToBHoM(pullSettings));
+            aPointList.Add(aXYZ_3.ToBHoM(pullSettings));
+            aPointList.Add(aXYZ_4.ToBHoM(pullSettings));
+            aPointList.Add(aXYZ_1.ToBHoM(pullSettings));
+
+            return BH.Engine.Geometry.Create.Polyline(aPointList);
         }
 
         /***************************************************/
