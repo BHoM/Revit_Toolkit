5d1e8106a1d50f64ff0cc564f404a8372c053efa
Jakub Ziolkowski <Jakub.Ziolkowski@burohappold.com>
2018-10-15 11:45:23 +0100

    Updated pull for Environmental BuildingElement

---
+++ Engine_Cobra/Query/PolyCurves.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+using Autodesk.Revit.DB;
+
+using BH.Engine.Geometry;
+using BH.oM.Adapters.Revit.Settings;
+
+namespace BH.UI.Cobra.Engine
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static List<oM.Geometry.PolyCurve> PolyCurves(this Face face, PullSettings pullSettings = null)
+        {
+            List<oM.Geometry.PolyCurve> aResult = new List<oM.Geometry.PolyCurve>();
+
+            foreach(EdgeArray aEdgeArray in face.EdgeLoops)
+            {
+                List<oM.Geometry.ICurve> aCurveList = Convert.ToBHoM(aEdgeArray, pullSettings);
+                aResult.Add(Create.PolyCurve(aCurveList));
+            }
+
+            return aResult;
+        }
+
+        /***************************************************/
+    }
+}
