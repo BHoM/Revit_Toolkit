7da9eb01121a2a4d2e171abd9cb97ca621cece32
Jakub Ziolkowski <Jakub.Ziolkowski@burohappold.com>
2018-10-19 12:14:57 +0100

    SetTag added and Create method for Adapter_Cobra updated

---
+++ Revit_Engine/Modify/SetTag.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+using BH.oM.Base;
+
+namespace BH.Engine.Adapters.Revit
+{
+    public static partial class Modify
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static IBHoMObject SetTag(this IBHoMObject bHoMObject, string tag)
+        {
+            if (bHoMObject == null)
+                return null;
+
+            IBHoMObject aIBHoMObject = bHoMObject.GetShallowClone();
+
+            if (aIBHoMObject.Tags == null)
+                aIBHoMObject.Tags = new HashSet<string>();
+
+            aIBHoMObject.Tags.Add(tag);
+
+            return aIBHoMObject;
+        }
+
+        /***************************************************/
+    }
+}
