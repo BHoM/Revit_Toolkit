cca19181e8568b4213c83da5b889794fb5820949
Jakub Ziolkowski <Jakub.Ziolkowski@BuroHappold.com>
2018-11-14 12:04:27 +0000

    IsShade method for BuildingElement added

---
+++ Revit_Engine/Query/IsShade.cs
@@ -0,0 +1,40 @@
+using System.ComponentModel;
+
+using BH.oM.Base;
+using BH.oM.Reflection.Attributes;
+using BH.oM.Environment.Elements;
+
+namespace BH.Engine.Adapters.Revit
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Cheks whatever Building Element is shade element. Works only for Building Elements pulled from analytical model and adjacency have been assigned.")]
+        [Input("buildingElement", "BuildingElement pulled from Revit analytical model")]
+        [Output("IsShade")]
+        public static bool IsShade(this BuildingElement buildingElement)
+        {
+            if (buildingElement == null)
+                return false;
+
+            if (buildingElement.CustomData == null)
+                return false;
+
+            if (!buildingElement.CustomData.ContainsKey(Convert.SpaceId))
+                return false;
+
+            if (!buildingElement.CustomData.ContainsKey(Convert.AdjacentSpaceId))
+                return false;
+
+            int aSpaceId = buildingElement.SpaceId();
+            int aAdjacentSpaceId = buildingElement.AdjacentSpaceId();
+
+            return aSpaceId == -1 && aAdjacentSpaceId == -1;
+        }
+
+        /***************************************************/
+    }
+}
