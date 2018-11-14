6767dacbe1ef01cb25fbba9c3f1b208ada52cedb
Jakub Ziolkowski <Jakub.Ziolkowski@BuroHappold.com>
2018-11-14 10:46:14 +0000

    Adjacent Space Id Parameter added to CustomData, IsInternal IsExternal methods for BuildingElement implemented

---
+++ Revit_Engine/Query/AdjacentSpaceId.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+
+using BH.oM.Base;
+using BH.oM.Reflection.Attributes;
+
+namespace BH.Engine.Adapters.Revit
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("gets integer representation of adjacent Space ElementId (stored in CustomData) for given BHoMObject.")]
+        [Input("bHoMObject", "BHoMObject")]
+        [Output("ElementId")]
+        public static int AdjacentSpaceId(this IBHoMObject bHoMObject)
+        {
+            if (bHoMObject == null)
+                return -1;
+
+            object aValue = null;
+            if (bHoMObject.CustomData.TryGetValue(Convert.AdjacentSpaceId, out aValue))
+            {
+                if (aValue is string)
+                {
+                    int aInt = -1;
+                    if (int.TryParse((string)aValue, out aInt))
+                        return aInt;
+                }
+                else if (aValue is int)
+                {
+                    return (int)aValue;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+
+            return -1;
+        }
+
+        /***************************************************/
+    }
+}
