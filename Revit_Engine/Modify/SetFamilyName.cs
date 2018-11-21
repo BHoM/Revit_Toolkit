a9c4030b0d6d9361029b4bdc9627d83bd2781eb0
Jakub Ziolkowski <Jakub.Ziolkowski@burohappold.com>
2018-11-21 11:21:37 +0000

    SetFamilyName and SetFamilyTypeName added. Elevated roofs handing added

---
+++ Revit_Engine/Modify/SetFamilyName.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+using BH.oM.Reflection.Attributes;
+using BH.oM.Environment.Properties;
+
+namespace BH.Engine.Adapters.Revit
+{
+    public static partial class Modify
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Sets Family Type name for Builiding Element Properties")]
+        [Input("buildingElementProperties", "Building Element Properties")]
+        [Input("familyTypeName", "Revit Family Type Name")]
+        [Output("BuildingElementProperties")]
+        public static BuildingElementProperties SetFamilyTypeName(this BuildingElementProperties buildingElementProperties, string familyTypeName)
+        {
+            if (buildingElementProperties == null)
+                return null;
+
+            BuildingElementProperties aBuildingElementProperties = buildingElementProperties.GetShallowClone() as BuildingElementProperties;
+
+            if (aBuildingElementProperties.CustomData.ContainsKey(Convert.FamilyTypeName))
+                aBuildingElementProperties.CustomData[Convert.FamilyTypeName] = familyTypeName;
+            else
+                aBuildingElementProperties.CustomData.Add(Convert.FamilyTypeName, familyTypeName);
+
+            return aBuildingElementProperties;
+        }
+
+        /***************************************************/
+    }
+}
