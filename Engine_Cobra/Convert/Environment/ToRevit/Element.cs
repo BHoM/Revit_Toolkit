1c3fd0ec1ac4516696ab4d7c2c4359677cb73f3d
Jakub Ziolkowski <Jakub.Ziolkowski@BuroHappold.com>
2018-12-07 10:19:41 +0000

    Minor updates for BuildingElement Torevit metod (Level selection changed)

--- Engine_Cobra/Convert/Environment/ToRevit/Element.cs
+++ Engine_Cobra/Convert/Environment/ToRevit/Element.cs
@@ -29,13 +29,13 @@ namespace BH.UI.Cobra.Engine
 
             ElementType aElementType = null;
 
-            if(buildingElement.BuildingElementProperties != null)
+            if (buildingElement.BuildingElementProperties != null)
                 aElementType = buildingElement.BuildingElementProperties.ToRevitElementType(document, pushSettings);
-            
-            if(aElementType == null)
+
+            if (aElementType == null)
             {
                 string aFamilyTypeName = BH.Engine.Adapters.Revit.Query.FamilyTypeName(buildingElement);
-                if(!string.IsNullOrEmpty(aFamilyTypeName))
+                if (!string.IsNullOrEmpty(aFamilyTypeName))
                 {
                     List<ElementType> aElementTypeList = new FilteredElementCollector(document).OfClass(typeof(ElementType)).Cast<ElementType>().ToList().FindAll(x => x.Name == aFamilyTypeName && x.Category != null);
                     if (aElementTypeList != null || aElementTypeList.Count() != 0)
@@ -68,6 +68,8 @@ namespace BH.UI.Cobra.Engine
             if (aBuildingElementType == null || !aBuildingElementType.HasValue)
                 return null;
 
+            Level aLevel = null;
+
             BuiltInParameter[] aBuiltInParameters = null;
             switch (aBuildingElementType.Value)
             {
@@ -75,53 +77,48 @@ namespace BH.UI.Cobra.Engine
                     //TODO: Create Ceiling from BuildingElement
                     break;
                 case BuildingElementType.Floor:
-                    if (aElementType != null)
-                    {
-                        Level aLevel = document.Level(buildingElement.MinimumLevel(), true);
-                        aElement = document.Create.NewFloor((buildingElement.PanelCurve as PolyCurve).ToRevitCurveArray(pushSettings), aElementType as FloorType, aLevel, false);
-                    }
-                    else
-                    {
-                        aElement = document.Create.NewFloor((buildingElement.PanelCurve as PolyCurve).ToRevitCurveArray(pushSettings), false);
-                    }  
+                    aLevel = document.Level(buildingElement.MinimumLevel(), true);
+                    aElement = document.Create.NewFloor((buildingElement.PanelCurve as PolyCurve).ToRevitCurveArray(pushSettings), aElementType as FloorType, aLevel, false);
+
                     aBuiltInParameters = new BuiltInParameter[] { BuiltInParameter.LEVEL_PARAM };
                     break;
                 case BuildingElementType.Roof:
                     ModelCurveArray aModelCurveArray = new ModelCurveArray();
-                    if (aElementType != null)
+                    aLevel = document.Level(buildingElement.MinimumLevel(), true);
+                    if (aLevel == null)
+                        break;
+
+                    Level aLevel_Temp = aLevel.NextLevel();
+                    if (aLevel_Temp != null)
+                        aLevel = aLevel_Temp;
+
+                    double aElevation = aLevel.Elevation;
+                    if (pushSettings.ConvertUnits)
+                        aElevation = UnitUtils.ConvertFromInternalUnits(aElevation, DisplayUnitType.DUT_METERS);
+
+                    oM.Geometry.Plane aPlane = BH.Engine.Geometry.Create.Plane(BH.Engine.Geometry.Create.Point(0, 0, aElevation), BH.Engine.Geometry.Create.Vector(0, 0, 1));
+                    ICurve aCurve = BH.Engine.Geometry.Modify.Project(buildingElement.PanelCurve as dynamic, aPlane) as ICurve;
+                    CurveArray aCurveArray = (aCurve as PolyCurve).ToRevitCurveArray(pushSettings);
+                    FootPrintRoof aFootPrintRoof = document.Create.NewFootPrintRoof(aCurveArray, aLevel, aElementType as RoofType, out aModelCurveArray);
+                    if (aFootPrintRoof != null)
                     {
-                        Level aLevel = document.Level(buildingElement.MinimumLevel(), true);
-                        double aElevation = aLevel.Elevation;
-                        if (pushSettings.ConvertUnits)
-                            aElevation = UnitUtils.ConvertFromInternalUnits(aElevation, DisplayUnitType.DUT_METERS);
-
-                        oM.Geometry.Plane aPlane = BH.Engine.Geometry.Create.Plane(BH.Engine.Geometry.Create.Point(0, 0, aElevation), BH.Engine.Geometry.Create.Vector(0, 0, 1));
-                        ICurve aCurve = BH.Engine.Geometry.Modify.Project(buildingElement.PanelCurve as dynamic, aPlane) as ICurve;
-                        CurveArray aCurveArray = (aCurve as PolyCurve).ToRevitCurveArray(pushSettings);
-                        FootPrintRoof aFootPrintRoof = document.Create.NewFootPrintRoof(aCurveArray, aLevel, aElementType as RoofType, out aModelCurveArray);
-                        if(aFootPrintRoof != null)
+                        List<ICurve> aCurveList = (buildingElement.PanelCurve as PolyCurve).Curves;
+                        if (aCurveList != null && aCurveList.Count > 0)
                         {
-                            List<ICurve> aCurveList = (buildingElement.PanelCurve as PolyCurve).Curves;
-                            if (aCurveList != null && aCurveList.Count > 0)
-                            {
-                                SlabShapeEditor aSlabShapeEditor = aFootPrintRoof.SlabShapeEditor;
-                                aSlabShapeEditor.ResetSlabShape();
+                            SlabShapeEditor aSlabShapeEditor = aFootPrintRoof.SlabShapeEditor;
+                            aSlabShapeEditor.ResetSlabShape();
 
-                                List<oM.Geometry.Point> aPointList = new List<oM.Geometry.Point>();
-                                foreach(ICurve aCurve_Temp in aCurveList)
-                                    aSlabShapeEditor.DrawPoint(BH.Engine.Geometry.Query.IStartPoint(aCurve_Temp).ToRevit(pushSettings));
-                            }
-
-                            aElement = aFootPrintRoof;
+                            List<oM.Geometry.Point> aPointList = new List<oM.Geometry.Point>();
+                            foreach (ICurve aCurve_Temp in aCurveList)
+                                aSlabShapeEditor.DrawPoint(BH.Engine.Geometry.Query.IStartPoint(aCurve_Temp).ToRevit(pushSettings));
                         }
+
+                        aElement = aFootPrintRoof;
                     }
-                        
                     break;
                 case BuildingElementType.Wall:
-                    aElement = Wall.Create(document, Convert.ToRevitCurveList(buildingElement.Curve(), pushSettings), false); //(document, ToRevitCurve(aICurve, pushSettings), aLevel.Id, false);
-                    if (aElementType != null)
-                        aElement.ChangeTypeId(aElementType.Id);
-
+                    aLevel = document.Level(buildingElement.MinimumLevel(), true);
+                    aElement = Wall.Create(document, Convert.ToRevitCurveList(buildingElement.Curve(), pushSettings), aElementType.Id, aLevel.Id, false); //(document, ToRevitCurve(aICurve, pushSettings), aLevel.Id, false);
                     aBuiltInParameters = new BuiltInParameter[] { BuiltInParameter.WALL_BASE_CONSTRAINT };
                     break;
                 case BuildingElementType.Door:
---
+++ Engine_Cobra/Query/NextLevel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Autodesk.Revit.DB;
+
+using BH.oM.Base;
+using BH.oM.Adapters.Revit.Settings;
+
+namespace BH.UI.Cobra.Engine
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+        
+        static public Level NextLevel(this Level Level)
+        {
+            if (Level == null)
+                return null;
+
+            List<Level> aLevelList = new FilteredElementCollector(Level.Document).OfClass(typeof(Level)).Cast<Level>().ToList();
+            if (aLevelList == null || aLevelList.Count < 2)
+                return null;
+
+            aLevelList.Sort((x, y) => x.ProjectElevation.CompareTo(y.ProjectElevation));
+
+            int aIndex = aLevelList.FindIndex(x => x.Id == Level.Id);
+            if (aIndex == -1)
+                return null;
+
+            if (aIndex == aLevelList.Count - 1)
+                return null;
+
+            return aLevelList[aIndex + 1];
+        }
+
+        /***************************************************/
+    }
+}
