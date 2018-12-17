b98296fd318d6f97f3a964c76763ab838fc5143b
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2018-12-17 12:19:40 +0000

    Fix #191 - change custom data keys

--- Revit_Engine/Convert/AdapterID.cs
+++ Revit_Engine/Convert/AdapterID.cs
@@ -9,8 +9,8 @@
         public const string AdapterId = "Revit_id";
         public const string ElementId = "Revit_elementId";
         public const string WorksetId = "Revit_worksetId";
-        public const string SpaceId = "Revit_spaceId";
-        public const string AdjacentSpaceId = "Revit_adjacentSpaceId";
+        public const string SpaceId = "SpaceID"; //FG Change per #191 - ongoing discussion on best use of this still ongoing
+        public const string AdjacentSpaceId = "AdjacentSpaceID"; //FG Change per #191 - ongoing discussion on best use of this still ongoing
         public const string FamilyName = "Revit_familyName";
         public const string FamilyTypeName = "Revit_familyTypeName";
         public const string CategoryName = "Revit_categoryName";
