039525d171fd1087a3ca3c7bd92cc23fd24fc2f1
Jakub Ziolkowski <Jakub.Ziolkowski@burohappold.com>
2018-12-19 12:07:58 +0000

    Display Errors only fix (Errors downgraded to Warnings)

---
+++ Engine_BHoMRevit/Compute/Notes.cs
@@ -0,0 +1,47 @@
+﻿using Autodesk.Revit.DB;
+using BH.oM.Base;
+using BH.oM.Adapters.Revit.Settings;
+
+
+namespace BH.UI.Revit.Engine
+{
+    public static partial class Compute
+    {
+        /***************************************************/
+        /****               Public methods              ****/
+        /***************************************************/
+
+        public static PullSettings DefaultIfNull(this PullSettings pullSettings)
+        {
+            if (pullSettings == null)
+            {
+                BH.Engine.Reflection.Compute.RecordNote("Pull settings are not set. Default settings are used.");
+                return PullSettings.Default;
+            }
+
+            return pullSettings;
+        }
+
+        /***************************************************/
+
+        public static PushSettings DefaultIfNull(this PushSettings pushSettings)
+        {
+            if (pushSettings == null)
+            {
+                BH.Engine.Reflection.Compute.RecordNote("Push settings are not set. Default settings are used.");
+                return PushSettings.Default;
+            }
+
+            return pushSettings;
+        }
+
+
+        /***************************************************/
+        /****             Internal methods              ****/
+        /***************************************************/
+
+        /***************************************************/
+
+
+    }
+}
