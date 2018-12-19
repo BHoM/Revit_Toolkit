a1d48a3a9ca56097f98cafb495462fa913856b5d
Jakub Ziolkowski <Jakub.Ziolkowski@burohappold.com>
2018-12-19 12:07:58 +0000

    Display Errors only fix (Errors downgraded to Warnings)

--- Engine_BHoMRevit/Compute/Errors.cs
+++ Engine_BHoMRevit/Compute/Errors.cs
@@ -1,74 +1,20 @@
 ﻿using Autodesk.Revit.DB;
-using Autodesk.Revit.DB.Structure;
 using BH.oM.Base;
+using BH.oM.Adapters.Revit.Settings;
+using BH.oM.Structure.Elements;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace BH.UI.Revit.Engine
 {
     public static partial class Compute
     {
         /***************************************************/
-        /****             Internal methods              ****/
+        /****               Public methods              ****/
         /***************************************************/
-
-        internal static void NotConvertedError(this Element element)
-        {
-            string aMessage = "Revit element could not be converted because conversion method does not exist.";
-
-            if (element != null)
-                aMessage = string.Format("{0} Element Id: {1}, Element Name: {2}", aMessage, element.Id.IntegerValue, element.Name);
-
-            BH.Engine.Reflection.Compute.RecordError(aMessage);
-        }
 
         /***************************************************/
 
-        internal static void NotConvertedError(this Document document)
-        {
-            string aMessage = "Revit document could not be converted because conversion method does not exist.";
-
-            if (document != null)
-                aMessage = string.Format("{0} Document title: {1}", aMessage, document.Title);
-
-            BH.Engine.Reflection.Compute.RecordError(aMessage);
-        }
-
-        /***************************************************/
-
-        internal static void NotConvertedError(this StructuralMaterialType structuralMaterialType)
-        {
-            BH.Engine.Reflection.Compute.RecordError("Structural meterial type " + structuralMaterialType + " could not be converted because conversion method does not exist.");
-        }
-
-        /***************************************************/
-
-        internal static void CheckIfNullPull(this Element element)
-        {
-            if (element == null)
-                BH.Engine.Reflection.Compute.RecordError("BHoM object could not be read because Revit element does not exist.");
-        }
-
-        /***************************************************/
-
-        internal static void CheckIfNullPush(this Element element, IBHoMObject BHoMObject)
-        {
-            if (element == null)
-                BH.Engine.Reflection.Compute.RecordError(string.Format("Revit element has not been created due to BHoM/Revit conversion issues. BHoM element Guid: {0}", BHoMObject.BHoM_Guid));
-        }
-
-        /***************************************************/
-
-        internal static void NullDocumentError()
-        {
-            BH.Engine.Reflection.Compute.RecordError("BHoM object could not be read because Revit document does not exist.");
-        }
-
-        /***************************************************/
-
-        internal static void NullObjectError()
-        {
-            BH.Engine.Reflection.Compute.RecordError("BHoM object could not be created becasue Revit object is null.");
-        }
-
-        /***************************************************/
     }
 }
--- Engine_BHoMRevit/Compute/Warnings.cs
+++ Engine_BHoMRevit/Compute/Warnings.cs
@@ -1,6 +1,6 @@
 ﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
 using BH.oM.Base;
-using BH.oM.Adapters.Revit.Settings;
 using BH.oM.Structure.Elements;
 using System;
 using System.Collections.Generic;
@@ -11,36 +11,68 @@ namespace BH.UI.Revit.Engine
     public static partial class Compute
     {
         /***************************************************/
-        /****               Public methods              ****/
-        /***************************************************/
-
-        public static PullSettings DefaultIfNull(this PullSettings pullSettings)
-        {
-            if (pullSettings == null)
-            {
-                BH.Engine.Reflection.Compute.RecordWarning("Pull settings are not set. Default settings are used.");
-                return PullSettings.Default;
-            }
-
-            return pullSettings;
-        }
-
-        /***************************************************/
-
-        public static PushSettings DefaultIfNull(this PushSettings pushSettings)
-        {
-            if (pushSettings == null)
-            {
-                BH.Engine.Reflection.Compute.RecordWarning("Push settings are not set. Default settings are used.");
-                return PushSettings.Default;
-            }
-
-            return pushSettings;
-        }
-
-
-        /***************************************************/
         /****             Internal methods              ****/
+        /***************************************************/
+
+        internal static void NotConvertedError(this Element element)
+        {
+            string aMessage = "Revit element could not be converted because conversion method does not exist.";
+
+            if (element != null)
+                aMessage = string.Format("{0} Element Id: {1}, Element Name: {2}", aMessage, element.Id.IntegerValue, element.Name);
+
+            BH.Engine.Reflection.Compute.RecordWarning(aMessage);
+        }
+
+        /***************************************************/
+
+        internal static void NotConvertedError(this Document document)
+        {
+            string aMessage = "Revit document could not be converted because conversion method does not exist.";
+
+            if (document != null)
+                aMessage = string.Format("{0} Document title: {1}", aMessage, document.Title);
+
+            BH.Engine.Reflection.Compute.RecordWarning(aMessage);
+        }
+
+        /***************************************************/
+
+        internal static void NotConvertedError(this StructuralMaterialType structuralMaterialType)
+        {
+            BH.Engine.Reflection.Compute.RecordWarning("Structural meterial type " + structuralMaterialType + " could not be converted because conversion method does not exist.");
+        }
+
+        /***************************************************/
+
+        internal static void CheckIfNullPull(this Element element)
+        {
+            if (element == null)
+                BH.Engine.Reflection.Compute.RecordWarning("BHoM object could not be read because Revit element does not exist.");
+        }
+
+        /***************************************************/
+
+        internal static void CheckIfNullPush(this Element element, IBHoMObject BHoMObject)
+        {
+            if (element == null)
+                BH.Engine.Reflection.Compute.RecordWarning(string.Format("Revit element has not been created due to BHoM/Revit conversion issues. BHoM element Guid: {0}", BHoMObject.BHoM_Guid));
+        }
+
+        /***************************************************/
+
+        internal static void NullDocumentError()
+        {
+            BH.Engine.Reflection.Compute.RecordWarning("BHoM object could not be read because Revit document does not exist.");
+        }
+
+        /***************************************************/
+
+        internal static void NullObjectError()
+        {
+            BH.Engine.Reflection.Compute.RecordWarning("BHoM object could not be created becasue Revit object is null.");
+        }
+
         /***************************************************/
 
         internal static void LogNullProperties(this BHoMObject obj, IEnumerable<string> propertyNames = null)
