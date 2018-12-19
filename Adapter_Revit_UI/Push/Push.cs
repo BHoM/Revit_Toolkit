2b5af55a57b9d643e3be4f10eda56839057b9d3e
Jakub Ziolkowski <Jakub.Ziolkowski@burohappold.com>
2018-12-19 16:10:17 +0000

    BHoMRevit renamed to Revit_UI

---
+++ Adapter_Revit_UI/Push/Push.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using BH.oM.Base;
+
+namespace BH.UI.Revit.Adapter
+{
+    public partial class RevitUIAdapter
+    {
+
+        /***************************************************/
+        /**** Protected Methods                         ****/
+        /***************************************************/
+
+        public override List<IObject> Push(IEnumerable<IObject> objects, string tag = "", Dictionary<string, object> config = null)
+        {
+            bool aSuccess = true;
+
+            List<IObject> objectsToPush = Config.CloneBeforePush ? objects.Select(x => x is BHoMObject ? ((BHoMObject)x).GetShallowClone() : x).ToList() : objects.ToList(); //ToList() necessary for the return collection to function properly for cloned objects
+
+            Type iBHoMObjectType = typeof(IBHoMObject);
+            MethodInfo miToList = typeof(Enumerable).GetMethod("Cast");
+            List<IObject> aResult = new List<IObject>();
+            foreach (var typeGroup in objectsToPush.GroupBy(x => x.GetType()))
+            {
+                MethodInfo miListObject = miToList.MakeGenericMethod(new[] { typeGroup.Key });
+
+                var list = miListObject.Invoke(typeGroup, new object[] { typeGroup });
+
+                if (iBHoMObjectType.IsAssignableFrom(typeGroup.Key))
+                    aSuccess &= Create(list as dynamic);                    
+            }
+
+            return aSuccess ? objectsToPush : new List<IObject>();
+        }
+
+    }
+}
---
+++ Adapter_Revit_UI/Types/DependencyTypes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+using BH.oM.Adapters.Revit.Elements;
+
+namespace BH.UI.Revit.Adapter
+{
+    public partial class RevitUIAdapter
+    {
+        /***************************************************/
+        /**** BHoM Adapter Interface                    ****/
+        /***************************************************/
+
+        protected override List<Type> DependencyTypes<T>()
+        {
+            Type type = typeof(T);
+
+            if (m_DependencyTypes.ContainsKey(type))
+                return m_DependencyTypes[type];
+
+            else if (m_DependencyTypes.ContainsKey(type.BaseType))
+                return m_DependencyTypes[type.BaseType];
+
+            else
+            {
+                foreach (Type interType in type.GetInterfaces())
+                {
+                    if (m_DependencyTypes.ContainsKey(interType))
+                        return m_DependencyTypes[interType];
+                }
+            }
+
+
+            return new List<Type>();
+        }
+
+
+        /***************************************************/
+        /**** Private Fields                            ****/
+        /***************************************************/
+
+        private static Dictionary<Type, List<Type>> m_DependencyTypes = new Dictionary<Type, List<Type>>
+        {
+            {typeof(Viewport), new List<Type> { typeof(Sheet), typeof(ViewPlan) } },
+            {typeof(Sheet), new List<Type> { typeof(ViewPlan)} }
+            //{typeof(ISectionProperty), new List<Type> { typeof(Material), typeof(IProfile) } },
+            //{typeof(PanelPlanar), new List<Type> { typeof(ISurfaceProperty), typeof(Level) } },
+            //{typeof(ISurfaceProperty), new List<Type> { typeof(Material) } }
+        };
+
+        /***************************************************/
+    }
+}
