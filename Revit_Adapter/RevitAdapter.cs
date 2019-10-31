a6e7462f57e3a9b63b5c8fc4bbb5695212765b61
Jakub Ziolkowski <Jakub.Ziolkowski@burohappold.com>
2019-10-31 17:46:41 +0000

    UpdateProperty Removed according to @alelom comments

--- Revit_Adapter/RevitAdapter.cs
+++ Revit_Adapter/RevitAdapter.cs
@@ -177,46 +177,6 @@ namespace BH.Adapter.Revit
 
         /***************************************************/
 
-        public override int UpdateProperty(FilterRequest filter, string property, object newValue, Dictionary<string, object> config = null)
-        {
-            //If internal adapter is loaded call it directly
-            if (InternalAdapter != null)
-            {
-                InternalAdapter.RevitSettings = RevitSettings;
-                return InternalAdapter.UpdateProperty(filter, property, newValue, config);
-            }
-
-            //Reset the wait event
-            m_waitEvent.Reset();
-
-
-            if (!CheckConnection())
-                return 0;
-
-            config = config == null ? new Dictionary<string, object>() : null;
-
-            //Send data through the socket link
-            m_linkIn.SendData(new List<object>() { PackageType.UpdateProperty, new Tuple<FilterRequest,string,object>(filter,property, newValue), config, RevitSettings });
-
-            //Wait until the return message has been recieved
-            if (!m_waitEvent.WaitOne(TimeSpan.FromMinutes(m_waitTime)))
-                Engine.Reflection.Compute.RecordError("The connection with Revit timed out. If working on a big model, try to increase the max wait time");
-
-            int returnValue = 0;
-            //Grab the return objects from the latest package
-            if (m_returnPackage.Count > 0 && m_returnPackage[0] is int)
-                returnValue = (int)m_returnPackage[0];
-
-            //Clear the return list
-            m_returnPackage.Clear();
-
-            //Raise returned events
-            RaiseEvents();
-
-            //Return the package
-            return returnValue;
-        }
-
         public bool IsValid()
         {
             if (m_linkIn == null || m_linkOut == null)
