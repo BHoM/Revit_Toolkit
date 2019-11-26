75fac4a3ce75efd8a02892aedf0ad1cb2fdbf6d5
Pawel Baran <pawel.baran@burohappold.com>
2019-11-26 14:25:44 +0100

    AssemblyInfo files updated

--- Revit_Adapter/Properties/AssemblyInfo.cs
+++ Revit_Adapter/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2018, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -27,11 +27,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Revit_ExternalAdapter")]
+[assembly: AssemblyTitle("Revit_External_Adapter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Revit_ExternalAdapter")]
+[assembly: AssemblyProduct("Revit_External_Adapter")]
 [assembly: AssemblyCopyright("Copyright © https://github.com/BHoM")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
