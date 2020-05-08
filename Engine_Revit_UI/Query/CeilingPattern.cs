dd355f8e5b726f19f0a239db8630429e4f49edcc
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-05-08 19:52:12 +0100

    Add copyright

--- Engine_Revit_UI/Query/CeilingPattern.cs
+++ Engine_Revit_UI/Query/CeilingPattern.cs
@@ -20,7 +20,7 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
- System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
