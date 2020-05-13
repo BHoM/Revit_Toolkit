cf80a0e17e72720a9cfce2a782265b794dc75922
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-05-13 10:09:10 +0100

    More revisions

--- Engine_Revit_UI/Convert/Architecture/FromRevit/Ceilings.cs
+++ Engine_Revit_UI/Convert/Architecture/FromRevit/Ceilings.cs
@@ -78,8 +78,8 @@ namespace BH.UI.Revit.Engine
                 newCeiling.UpdateValues(settings, ceiling);
 
                 newCeiling.CustomData.Add(BH.Engine.Adapters.Revit.Convert.CeilingPattern, ceiling.CeilingPattern(settings, planarSurface));
-                if (disctionary.Values.Sum(x => x.Count) != 0)
-                    BH.Engine.Reflection.Compute.RecordWarning("Currently ceiling openings are not taken into account when generating ceiling patterns.");
+                if (disctionary.Values.Where(x => x != null).Sum(x => x.Count) != 0)
+                    BH.Engine.Reflection.Compute.RecordWarning("Currently ceiling openings are not taken into account when generating ceilings.");
 
                 ceilingList.Add(newCeiling);
             }
