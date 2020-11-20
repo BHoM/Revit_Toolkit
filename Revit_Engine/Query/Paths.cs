907c6accc154d9e31e35acab64ef67b2922bdff5
Pawel Baran <pawel.baran@burohappold.com>
2020-11-20 19:43:31 +0100

    code simplified

--- Revit_Engine/Query/Paths.cs
+++ Revit_Engine/Query/Paths.cs
@@ -33,30 +33,16 @@ namespace BH.Engine.Adapters.Revit
         /***************************************************/
         /****              Public methods               ****/
         /***************************************************/
-
+        
         [Description("Returns all file paths in FamilyLibrary that meet given Revit category, family and family type requirements.")]
         [Input("familyLibrary", "FamilyLibrary to be queried.")]
         [Input("categoryName", "Name of Revit category to be sought for. Optional: if null, all items to be taken.")]
         [Input("familyName", "Name of Revit family to be sought for. Optional: if null, all items to be taken.")]
         [Input("typeName", "Name of Revit family type to be sought for. Optional: if null, all items to be taken.")]
         [Output("paths")]
-        public static IEnumerable<string> Paths(this FamilyLibrary familyLibrary, string categoryName = null, string familyName = null, string typeName = null)
+        public static List<string> Paths(this FamilyLibrary familyLibrary, string categoryName = null, string familyName = null, string typeName = null)
         {
-            if (familyLibrary?.Files == null)
-                return null;
-
-            IEnumerable<RevitFilePreview> files = familyLibrary?.Files;
-
-            if (!string.IsNullOrWhiteSpace(categoryName))
-                files = files.Where(x => x.CategoryName == categoryName);
-
-            if (!string.IsNullOrWhiteSpace(familyName))
-                files = files.Where(x => x.FamilyName == familyName);
-
-            if (!string.IsNullOrWhiteSpace(typeName))
-                files = files.Where(x => x.FamilyTypeNames.Any(y => y == typeName));
-
-            return files.Select(x => x.Path);
+            return familyLibrary.RevitFilePreviews()?.Select(x => x.Path).ToList();
         }
 
         /***************************************************/
