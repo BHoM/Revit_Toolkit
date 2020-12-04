2b837a5369ed4130a2c2ef635b54ce38b6d016f9
Pawel Baran <pawel.baran@burohappold.com>
2020-12-04 12:48:30 +0100

    transform converts fixed

--- Revit_Core_Engine/Convert/Geometry/ToRevit/Transform.cs
+++ Revit_Core_Engine/Convert/Geometry/ToRevit/Transform.cs
@@ -36,11 +36,11 @@ namespace BH.Revit.Engine.Core
         {
             if (transformMatrix == null)
                 return null;
-
-            XYZ translation = new XYZ(transformMatrix.Matrix[3, 0].FromSI(UnitType.UT_Length), transformMatrix.Matrix[3, 1].FromSI(UnitType.UT_Length), transformMatrix.Matrix[3, 2].FromSI(UnitType.UT_Length));
+            
             XYZ basisX = new XYZ(transformMatrix.Matrix[0, 0], transformMatrix.Matrix[1, 0], transformMatrix.Matrix[2, 0]);
             XYZ basisY = new XYZ(transformMatrix.Matrix[0, 1], transformMatrix.Matrix[1, 1], transformMatrix.Matrix[2, 1]);
             XYZ basisZ = new XYZ(transformMatrix.Matrix[0, 2], transformMatrix.Matrix[1, 2], transformMatrix.Matrix[2, 2]);
+            XYZ translation = new XYZ(transformMatrix.Matrix[0, 3].FromSI(UnitType.UT_Length), transformMatrix.Matrix[1, 3].FromSI(UnitType.UT_Length), transformMatrix.Matrix[2, 3].FromSI(UnitType.UT_Length));
             Transform transform = Transform.CreateTranslation(translation);
             transform.set_Basis(0, basisX);
             transform.set_Basis(1, basisY);
