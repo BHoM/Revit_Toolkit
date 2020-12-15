7617790fe96ade5136e6724e71a9b94c275ca465
Pawel Baran <pawel.baran@burohappold.com>
2020-12-15 20:38:46 +0100

    Location update refined for IInstances

--- Revit_Core_Engine/Modify/Update.cs
+++ Revit_Core_Engine/Modify/Update.cs
@@ -104,7 +104,7 @@ namespace BH.Revit.Engine.Core
             if (!string.IsNullOrWhiteSpace(bHoMObject.Properties?.CategoryName) && element.Category?.Name != bHoMObject.Properties.CategoryName)
                 BH.Engine.Reflection.Compute.RecordWarning($"Updating the category of an existing Revit element is not allowed. Revit ElementId: {element.Id} BHoM_Guid: {bHoMObject.BHoM_Guid}");
 
-            if ((element.Host == null && bHoMObject.HostId != -1) || (element.Host != null && element.Host.Id.IntegerValue != bHoMObject.HostId))
+            if (((element.Host == null || element.Host is ReferencePlane) && bHoMObject.HostId != -1) || (element.Host != null && !(element.Host is ReferencePlane) && element.Host.Id.IntegerValue != bHoMObject.HostId))
                 BH.Engine.Reflection.Compute.RecordWarning($"Updating the host of an existing hosted element is not allowed. Revit ElementId: {element.Id} BHoM_Guid: {bHoMObject.BHoM_Guid}");
 
             return ((Element)element).Update(bHoMObject, settings, setLocationOnUpdate);
