af2a28720bda144f7d2184de9345bb592c80768c
Pawel Baran <pawel.baran@burohappold.com>
2020-12-15 20:37:16 +0100

    IsOnFace and IsInside added

---
+++ Revit_Core_Engine/Query/IsInside.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using Autodesk.Revit.DB;
+using System;
+
+namespace BH.Revit.Engine.Core
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /****              Public methods               ****/
+        /***************************************************/
+
+        public static bool IsInside(this XYZ point, Solid solid, double tolerance)
+        {
+            SolidCurveIntersectionOptions sco = new SolidCurveIntersectionOptions();
+            sco.ResultType = SolidCurveIntersectionMode.CurveSegmentsInside;
+
+            XYZ[] vectors = { XYZ.BasisX, XYZ.BasisY, XYZ.BasisZ };
+            foreach (XYZ vector in vectors)
+            {
+                Line l = Line.CreateBound(point - vector, point + vector);
+                SolidCurveIntersection sci = solid.IntersectWithCurve(l, sco);
+                if (sci == null)
+                    continue;
+
+                for (int i = 0; i < sci.SegmentCount; i++)
+                {
+                    Curve c = sci.GetCurveSegment(i);
+                    IntersectionResult ir = c.Project(point);
+                    if (ir != null && ir.Distance <= tolerance)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        /***************************************************/
+    }
+}
