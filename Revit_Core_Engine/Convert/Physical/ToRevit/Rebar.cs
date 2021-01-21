a56ce46623d81895e111812a3f352a6b83f78093
Łukasz Markowski <lukasz.markowski@burohappold.com>
2021-01-21 16:49:12 +0100

    ToRevit - alpha version
    rebasing

---
+++ Revit_Core_Engine/Convert/Physical/ToRevit/Rebar.cs
@@ -0,0 +1,134 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+using BH.Engine.Adapters.Revit;
+using BH.Engine.Geometry;
+using BH.Engine.Reflection;
+using BH.oM.Adapters.Revit.Settings;
+using BH.oM.Geometry;
+using BH.oM.Physical.Reinforcement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BH.Revit.Engine.Core
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /****            Interface methods              ****/
+        /***************************************************/
+
+        public static FamilyInstance IToRevitRebar(this IReinforcingBar reinforcement, Document document, RevitSettings settings = null, Dictionary<Guid, List<int>> refObjects = null)
+        {
+            return ToRevitRebar(reinforcement as dynamic, document, settings, refObjects);
+        }
+
+
+        /***************************************************/
+        /****              Public methods               ****/
+        /***************************************************/
+
+        public static Rebar ToRevitRebar(this PrimaryReinforcingBar bar, Document document, RevitSettings settings = null, Dictionary<Guid, List<int>> refObjects = null)
+        {
+            if (bar == null || document == null)
+                return null;
+
+            Rebar rebar = refObjects.GetValue<Rebar>(document, bar.BHoM_Guid);
+            if (rebar != null)
+                return rebar;
+
+            settings = settings.DefaultIfNull();
+
+            //getting bar type
+            string barTypeName = ((int)(bar.Diameter * 1000)).ToString();
+            RebarBarType barType = new FilteredElementCollector(document).OfClass(typeof(RebarBarType)).Where(x => x.Name == barTypeName).FirstOrDefault() as RebarBarType;
+            if (barType == null)
+            {
+                BH.Engine.Reflection.Compute.RecordError("Revit project does not contain adequate rebar family for one or more rebars.");
+                return null;
+            }
+
+            //getting host
+            Element host = null;
+            try
+            {
+                host = document.GetElement(new ElementId((int)bar.CustomData["host"]));
+            }
+            catch (KeyNotFoundException e)
+            {
+                BH.Engine.Reflection.Compute.RecordError("One or more rebars does not contain information about the host.");
+                return null;
+            }
+            if (host == null)
+            {
+                BH.Engine.Reflection.Compute.RecordError("One or more selected hosts does not exist in the Revit project.");
+                return null;
+            }
+
+            //creating rebar
+            RebarFreeFormValidationResult rffvr = new RebarFreeFormValidationResult();
+                        
+            List<List<Curve>> curves = new List<List<Curve>> { bar.CentreCurve.IToRevitCurves() };  //
+            IList<IList<Curve>> iListCurves = new List<IList<Curve>>();                             // Needed to convert
+            foreach (List<Curve> list in curves)                                                    // List<List<Curve>> -> IList<IList<Curve>>
+                iListCurves.Add(list);                                                              //
+
+            rebar = Rebar.CreateFreeForm(document, barType, host, iListCurves, out rffvr);
+
+
+            rebar.CopyParameters(bar, settings);
+            rebar.SetLocation(bar, settings);
+
+            refObjects.AddOrReplace(bar, rebar);
+            return rebar;
+        }
+
+        /***************************************************/
+
+        public static Rebar ToRevitRebar(this Stirrup reinforcement, Document document, RevitSettings settings = null, Dictionary<Guid, List<int>> refObjects = null)
+        {
+            if (reinforcement == null || document == null)
+                return null;
+
+            Rebar rebar = refObjects.GetValue<Rebar>(document, reinforcement.BHoM_Guid);
+            if (rebar != null)
+                return rebar;
+
+            settings = settings.DefaultIfNull();
+            
+            //     //
+            // WIP //
+            //     //
+
+            rebar.CopyParameters(reinforcement, settings);
+            rebar.SetLocation(reinforcement, settings);
+
+            refObjects.AddOrReplace(reinforcement, rebar);
+            return rebar;
+        }
+
+        /***************************************************/
+    }
+}
