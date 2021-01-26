9049101e66323ab6d260a930baf722ed1225b277
Pawel Baran <pawel.baran@burohappold.com>
2021-01-26 19:34:40 +0100

    #960 fixed

--- Revit_Core_Engine/Convert/Physical/FromRevit/Door.cs
+++ Revit_Core_Engine/Convert/Physical/FromRevit/Door.cs
@@ -57,9 +57,12 @@ namespace BH.Revit.Engine.Core
             if (location == null)
             {
                 if (host == null)
-                    BH.Engine.Reflection.Compute.RecordError(String.Format("Location of the door could not be retrieved from the model (possibly it has zero area or lies on a non-planar face). A door object without location has been returned. Revit ElementId: {0}", familyInstance.Id.IntegerValue));
+                    BH.Engine.Reflection.Compute.RecordWarning(String.Format("Location of the door could not be retrieved from the model (possibly it has zero area or lies on a non-planar face). A door object without location has been returned. Revit ElementId: {0}", familyInstance.Id.IntegerValue));
                 else
+                {
+                    BH.Engine.Reflection.Compute.RecordWarning(String.Format("Location of the door could not be retrieved from the model (possibly it has zero area or lies on a non-planar face), the opening has been skipped. Revit ElementId: {0}", familyInstance.Id.IntegerValue));
                     return null;
+                }
             }
 
             door = new Door { Location = location, Name = familyInstance.FamilyTypeFullName() };
