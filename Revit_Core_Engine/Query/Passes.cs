5604dc36b2449c183404668bc6b6c27d43c05188
Pawel Baran <pawel.baran@burohappold.com>
2021-03-15 18:48:06 +0100

    Passes prototype added

---
+++ Revit_Core_Engine/Query/Passes.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using Autodesk.Revit.DB;
+using BH.oM.Adapters.Revit.Requests;
+using BH.oM.Data.Requests;
+using System;
+
+namespace BH.Revit.Engine.Core
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /****              Public methods               ****/
+        /***************************************************/
+
+        public static bool IPasses(this Element element, IRequest request)
+        {
+            return Passes(element, request as dynamic);
+        }
+
+        /***************************************************/
+
+        public static bool Passes(this Element element, FilterByParameterText request)
+        {
+            if (request.TextComparisonType == oM.Adapters.Revit.Enums.TextComparisonType.Equal)
+                return element.LookupParameterString(request.ParameterName) == request.Value;
+
+            return false;
+        }
+
+        /***************************************************/
+
+        public static bool Passes(this Element element, FilterByCategory request)
+        {
+            string categoryName = element.Category.Name;
+            string soughtName = request.CategoryName;
+            if (!request.CaseSensitive)
+            {
+                categoryName = categoryName.ToLower();
+                soughtName = soughtName.ToLower();
+            }
+
+            return categoryName == soughtName;
+        }
+
+        /***************************************************/
+    }
+}
+
