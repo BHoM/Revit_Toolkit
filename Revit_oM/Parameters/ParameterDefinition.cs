2dc3fcc4fa9380fe968a7cdaf3278340914645ab
Pawel Baran <pawel.baran@burohappold.com>
2021-05-12 19:20:38 +0200

    ParameterDefinition object added

---
+++ Revit_oM/Parameters/ParameterDefinition.cs
@@ -0,0 +1,20 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Revit.Parameters
+{
+    public class ParameterDefinition : BHoMObject
+    {
+        public virtual string GroupName { get; set; } = ""; //TODO: only relevant to shared - split into 2 different? or merge this with paramGroup better!
+        public virtual string ParameterGroup { get; set; } = "";
+        public virtual string ParameterType { get; set; } = "";
+        public virtual bool Instance { get; set; } = true;
+        public virtual List<string> Categories { get; set; } = null;
+        public virtual bool Shared { get; set; } = false;
+        //TODO: add discipline!
+    }
+}
