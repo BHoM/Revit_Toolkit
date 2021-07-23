5854528d44382e79559e32cdcd0ad8b6aafcfb6d
Pawel Baran <pawel.baran@burohappold.com>
2021-07-23 13:49:12 +0200

    concept of FamilyMaps added to the oM

--- Revit_oM/Mapping/FamilyMap.cs
+++ Revit_oM/Mapping/FamilyMap.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * This file is part of the Buildings and Habitats object Model (BHoM)
  * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
  *
@@ -20,31 +20,25 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
-using BH.oM.Adapters.Revit.Mapping;
 using BH.oM.Base;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 
-namespace BH.oM.Adapters.Revit.Settings
+namespace BH.oM.Adapters.Revit.Mapping
 {
-    //[Description("An entity holding information about conversion-specific Revit parameter names as well as relationships between object property names (or names of RevitParameters attached to it) and Revit parameter names.")]
-    public class MappingSettings : BHoMObject
+    //[Description("An entity defining the relationship between object's property names (or names of RevitParameters attached to it) and parameter names of correspondent Revit elements.")]
+    public class FamilyMap : BHoMObject
     {
         /***************************************************/
         /****             Public Properties             ****/
         /***************************************************/
 
-        [Description("A collection of entities defining relationships between property names of BHoM types (or RevitParameters attached to objects of these types) and parameter names of correspondent Revit elements.")]
-        public virtual List<ParameterMap> ParameterMaps { get; set; } = new List<ParameterMap>();
+        //[Description("Type, which property names (or names of RevitParameters) are being mapped with Revit element parameters.")]
+        public virtual Type Type { get; set; } = null;
 
-        //[Description("A collection of entities defining relationships between property names of BHoM types (or RevitParameters attached to objects of these types) and parameter names of correspondent Revit elements.")]
-        public virtual List<string> FamilyMaps { get; set; } = new List<string>();
-
-        [Description("Name of the Revit parameter to be used as a source (on Pull) and target (on Push) of information for BHoM tags.")]
-        public virtual string TagsParameter { get; set; } = "BHE_Tags";
-
-        [Description("Name of the Revit parameter to be used as a source of information about material grade of a Revit element.")]
-        public virtual string MaterialGradeParameter { get; set; } = "BHE_Material Grade";
+        //[Description("A collection of type property names (or names of RevitParameters) and sets of their correspondent Revit parameter names.")]
+        public virtual List<string> FamilyNames { get; set; } = new List<string>();
 
         /***************************************************/
     }
