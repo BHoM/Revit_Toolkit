e7e8d14db420ca602880b44d23f512a152372f63
Pawel Baran <pawel.baran@burohappold.com>
2021-07-23 15:34:19 +0200

    MappedFamilyNames method added

---
+++ Revit_Engine/Query/MappedFamilyNames.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Adapters.Revit.Requests;
+using BH.oM.Adapters.Revit.Enums;
+using BH.oM.Data.Requests;
+using BH.oM.Reflection.Attributes;
+using System;
+using System.ComponentModel;
+using BH.Engine.Data;
+using System.Collections.Generic;
+using BH.oM.Adapters.Revit.Settings;
+using System.Linq;
+
+namespace BH.Engine.Adapters.Revit
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /****              Public methods               ****/
+        /***************************************************/
+
+        //[Description("Gets the discipline to which a given BHoM type belongs. The result is based on the namespace in which the type is declared, e.g. BH.oM.Structure.Elements.Bar will return oM.Adapters.Revit.Enums.Discipline.Structural.")]
+        //[Input("type", "BHoM type to be queried.")]
+        //[Output("discipline")]
+        public static List<string> MappedFamilyNames(this MappingSettings settings, Type bHoMType)
+        {
+            if (settings == null)
+                return null;
+
+            return settings.FamilyMaps.Where(x => x.Type == bHoMType).SelectMany(x => x.FamilyNames).ToList();
+        }
+
+        /***************************************************/
+    }
+}
+
