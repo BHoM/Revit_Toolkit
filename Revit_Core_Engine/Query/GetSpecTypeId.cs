cfe0e9427e895fb08e993e516a0d0552cdcd937b
Pawel Baran <pawel.baran@burohappold.com>
2021-08-16 12:58:20 +0200

    descriptions updated following @alelom's suggestion

--- Revit_Core_Engine/Query/GetSpecTypeId.cs
+++ Revit_Core_Engine/Query/GetSpecTypeId.cs
@@ -34,8 +34,8 @@ namespace BH.Revit.Engine.Core
         /****              Public methods               ****/
         /***************************************************/
 
-        [Description("A special-purpose method mimicking the Revit API method introduced in 2021 version, which extracts the equivalent of UnitType from a Revit parameter Definition.\n" +
-                     "It has been implemented to minimise the fallout caused by the API change on the existing code base of Revit_Toolkit.")]
+        [Description("This method is defined by BHoM only for Revit Versions < 2021. Revit versions from 2021 onwards define an equivalent method with the same name as part of their API." +
+                     "This BHoM implementation eliminates breaking changes between different Revit API versions. It returns the equivalent of UnitType from a Revit parameter Definition.")]
         [Input("definition", "Revit parameter Definition to extract the UnitType from.")]
         [Output("unitType", "UnitType extracted from the input Revit parameter Definition.")]
         public static UnitType GetSpecTypeId(this Definition definition)
