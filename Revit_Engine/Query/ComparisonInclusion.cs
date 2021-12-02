9c5982f83053fd0925c0aa02aabc46cf2162ff4a
Alessio Lombardi <alessio.lombardi@burohappold.com>
2021-12-02 13:15:32 +0000

    Imported from https://github.com/BHoM/Revit_Toolkit/pull/1109

---
+++ Revit_Engine/Query/ComparisonInclusion.cs
@@ -0,0 +1,81 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.Engine.Base;
+using BH.oM.Adapters.Revit;
+using BH.oM.Adapters.Revit.Elements;
+using BH.oM.Adapters.Revit.Parameters;
+using BH.oM.Base;
+using BH.oM.Diffing;
+using BH.oM.Reflection.Attributes;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace BH.Engine.Adapters.Revit
+{
+    public static partial class Query
+    {
+        [Description("When Diffing (Comparing objects), determine if and how RevitParameters should be considered.")]
+        [Input("parameter1", "First RevitParameter that is being compared.")]
+        [Input("parameter2", "Second RevitParameter that is being compared.")]
+        [Input("propertyFullName", "The Full Name of the RevitParameter stored on the object whose Comparison is being computed.")]
+        [Input("comparisonConfig", "Additional comparison configurations. You can specify a `ComparisonConfig` or a `RevitComparisonConfig`.")]
+        public static ComparisonInclusion ComparisonInclusion(this RevitParameter parameter1, RevitParameter parameter2, string propertyFullName, BaseComparisonConfig comparisonConfig)
+        {
+            ComparisonInclusion result = new ComparisonInclusion();
+            result.DisplayName = parameter1.Name + " (RevitParameter)"; // differences in any property of RevitParameters will be displayed like this.
+
+            // Check if we have a RevitComparisonConfig input.
+            RevitComparisonConfig rcc = comparisonConfig as RevitComparisonConfig;
+            if (rcc != null)
+            {
+                // Check the ParametersToConsider: if there is at least one name in the list, make sure that the current revitParameter's Name is contained in the list.
+                if ((rcc.ParametersToConsider?.Any() ?? false) && !rcc.ParametersToConsider.Any(ptc => parameter1.Name == ptc || parameter1.Name.WildcardMatch(ptc)))
+                {
+                    // The parameter is not within the ParametersToConsider.
+                    result.Include = false; // RevitParameter must be skipped
+                    return result;
+                }
+
+                // Check if the current revitParameter is within the ParametersExceptions.
+                if ((rcc.ParametersExceptions?.Any() ?? false) && rcc.ParametersExceptions.Any(ptc => parameter1.Name == ptc || parameter1.Name.WildcardMatch(ptc))) 
+                {
+                    result.Include = false; // RevitParameter must be skipped
+                    return result;
+                }
+
+                // Check the difference in the RevitParameters is a numerical difference, and if so whether it should be included given the input tolerances/significantFigures.
+                if (!BH.Engine.Diffing.Query.NumericalDifferenceInclusion(parameter1.Value, parameter2.Value, parameter1.Name, rcc.ParameterNumericTolerances, rcc.NumericTolerance, rcc.ParameterSignificantFigures, rcc.SignificantFigures))
+                {
+                    result.Include = false; // RevitParameter must be skipped
+                    return result;
+                }
+            }
+
+            return result; // pass the RevitParameter (do not skip it)
+        }
+    }
+}
+
+
---
+++ Revit_Engine/Query/HashString.cs
@@ -0,0 +1,85 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.Engine.Reflection;
+using BH.oM.Adapters.Revit;
+using BH.oM.Adapters.Revit.Elements;
+using BH.oM.Adapters.Revit.Parameters;
+using BH.oM.Base;
+using BH.oM.Diffing;
+using BH.oM.Reflection.Attributes;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace BH.Engine.Adapters.Revit
+{
+    public static partial class Query
+    {
+        [Description("Computes the Hash string for a Revit Parameter. This will then be used by the Hash algorithm to compute the overall Hash of an object owning Revit Parameters.")]
+        [Input("propertyFullName", "Full name of the RevitParameter property whose Hash is being computed. This name will be used to seek matches in the ComparisonConfig custom named tolerances/significant figures to determine if any custom numerical approximation should be done.")]
+        [Input("comparisonConfig", "Settings specified for this Hash computation. This can be a RevitComparisonConfig object.")]
+        public static string HashString(this RevitParameter revitParameter, string propertyFullName = null, BaseComparisonConfig comparisonConfig = null)
+        {
+            // Null check.
+            if (revitParameter == null)
+                return null;
+
+            string hashString = revitParameter.Name + revitParameter.Value;
+
+            // Check if we have a RevitComparisonConfig input.
+            RevitComparisonConfig rcc = comparisonConfig as RevitComparisonConfig;
+            if (rcc != null)
+            {
+                // If there is at least one name in the ParametersToConsider, make sure that the current revitParameter's Name is contained in the list.
+                if ((rcc.ParametersToConsider?.Any() ?? false) && !rcc.ParametersToConsider.Contains(revitParameter.Name))
+                    // The parameter is not within the ParametersToConsider. RevitParameter must be skipped
+                    return null;
+
+                // Check if the current revitParameter is within the ParametersExceptions.
+                if (rcc.ParametersExceptions?.Contains(revitParameter.Name) ?? false)
+                    // The parameter is not within the ParametersToConsider. RevitParameter must be skipped
+                    return null;
+
+                // If the RevitParameter is numeric, take care of custom tolerances/significant figures.
+                if (revitParameter.Value.GetType().IsNumeric())
+                {
+                    // If we didn't specify any custom tolerance/significant figures, just return the input.
+                    if (rcc.NumericTolerance == double.MinValue && rcc.SignificantFigures == int.MaxValue
+                        && (!rcc.ParameterNumericTolerances?.Any() ?? true) && (!rcc.ParameterSignificantFigures?.Any() ?? true))
+                        return hashString;
+
+                    // Otherwise, return the approximated value.
+                    return BH.Engine.Base.Query.NumericalApproximation(
+                        double.Parse(revitParameter.Value.ToString()), propertyFullName, rcc.ParameterNumericTolerances, rcc.NumericTolerance, rcc.ParameterSignificantFigures, rcc.SignificantFigures)
+                        .ToString();
+                }
+            }
+
+            // Pass the RevitParameter (do not skip it). Return a string that represents this RevitParameter.
+            return $"{revitParameter.Name}:{revitParameter.Value}";
+        }
+    }
+}
+
+
