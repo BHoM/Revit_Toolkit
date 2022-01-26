39e01c239e32afc62460dac34b93ab884a5933ce
Pawel Baran <pawel.baran@burohappold.com>
2022-01-26 15:55:11 +0100

    GetRevitElementType and GetRevitTypeParameterValue methods added

---
+++ Revit_Engine/Query/GetRevitTypeParameterValue.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Adapters.Revit.Parameters;
+using BH.oM.Base;
+using BH.oM.Base.Attributes;
+using System.ComponentModel;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BH.Engine.Adapters.Revit
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /****              Public methods               ****/
+        /***************************************************/
+
+        [Description("Retrieves value of a parameter from a Revit element type representation attached to a BHoM object pulled from Revit.")]
+        [Input("bHoMObject", "BHoMObject to which the parameters will be attached.")]
+        [Input("parameterName", "Name of the parameter to be sought for.")]
+        [Output("value")]
+        public static object GetRevitTypeParameterValue(this IBHoMObject bHoMObject, string parameterName)
+        {
+            if (bHoMObject == null)
+            {
+                BH.Engine.Base.Compute.RecordError("Could not extract Revit element type parameter value from a null BHoM object.");
+                return null;
+            }
+
+            return bHoMObject.IGetRevitElementType()?.GetRevitParameterValue(parameterName);
+        }
+
+        /***************************************************/
+    }
+}
+
+
+
