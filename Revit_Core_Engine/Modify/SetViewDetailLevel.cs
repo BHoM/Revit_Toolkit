3733ff747fff10577b2034649723987f07623b8f
Pawel Baran <pawel.baran@burohappold.com>
2022-03-21 23:05:08 +0100

    SetViewDetailLevel method tweaked

--- Revit_Core_Engine/Modify/SetViewDetailLevel.cs
+++ Revit_Core_Engine/Modify/SetViewDetailLevel.cs
@@ -37,22 +37,36 @@ namespace BH.Revit.Engine.Core
         [Description("Attempts to modify a view's Detail Level while checking if it's controlled by a view template.")]
         [Input("view", "The view to change the Detail Level.")]
         [Input("viewDetailLevel", "The Detail Level to change in the view.")]
-        [Output("setViewDetailLevel", "The modified view.")] 
-        public static void SetViewDetailLevel(this View view, ViewDetailLevel viewDetailLevel)
+        [Output("success", "True if the detail level has been successfully set to the view.")]
+        public static bool SetViewDetailLevel(this View view, ViewDetailLevel viewDetailLevel)
         {
-            if (view.ViewTemplateId != ElementId.InvalidElementId)
+            if (view == null)
             {
-                View viewTemplate = view.Document.GetElement(view.ViewTemplateId) as View;
-                Parameter detailLevelParameter = viewTemplate.get_Parameter(BuiltInParameter.VIEW_DETAIL_LEVEL);
-                if(detailLevelParameter.UserModifiable)
-                    view.DetailLevel = viewDetailLevel;
-                else
-                    BH.Engine.Base.Compute.RecordWarning("Could not change the view detail level because it's being controlled by the view template.");
+                BH.Engine.Base.Compute.RecordError("Cannot set detail level to a null view.");
+                return false;
             }
-            else
+
+            if (viewDetailLevel == ViewDetailLevel.Undefined)
             {
-                view.DetailLevel = viewDetailLevel;
+                BH.Engine.Base.Compute.RecordWarning("Detail level of a view cannot be set to Undefined.");
+                return false;
             }
+
+            Parameter detailLevelParameter = view.get_Parameter(BuiltInParameter.VIEW_DETAIL_LEVEL);
+            if (detailLevelParameter == null)
+            {
+                BH.Engine.Base.Compute.RecordWarning("Detail level of a view could not be set because the relevant parameter could not be found.");
+                return false;
+            }
+
+            if (detailLevelParameter.IsReadOnly)
+            {
+                BH.Engine.Base.Compute.RecordWarning("Detail level of a view could not be set because the relevant parameter is read only (possibly because it is controlled by the template).");
+                return false;
+            }
+
+            view.DetailLevel = viewDetailLevel;
+            return true;
         }
 
         /***************************************************/
