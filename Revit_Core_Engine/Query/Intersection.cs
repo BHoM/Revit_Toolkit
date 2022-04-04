898c89ddb403409a67d8cd4e5dfdd8d2587e8ce0
Michal Pekacki <michal.pekacki@burohappold.com>
2022-04-04 10:41:35 +0200

    methods added

---
+++ Revit_Core_Engine/Query/Intersection.cs
@@ -0,0 +1,91 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+
+using Autodesk.Revit.DB;
+using BH.oM.Base.Attributes;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace BH.Revit.Engine.Core
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /****              Public methods               ****/
+        /***************************************************/
+
+        [Description("Intersection result of two curves. If there is no intersection, an empty list is returned.")]
+        [Input("curve1", "First curve to check the intersection for.")]
+        [Input("curve2", "Second curve to check the intersection for.")]
+        [Output("points", "List of intersecting points.")]
+        public static List<XYZ> Intersection(this Curve curve1, Curve curve2)
+        {
+            if (curve1 == null || curve2 == null)
+            {
+                return null;
+            }
+
+            List<XYZ> points = new List<XYZ>();
+            curve1.Intersect(curve2, out IntersectionResultArray intersectionResultArray);
+
+            if (intersectionResultArray == null)
+            {
+                return points;
+            }
+
+            foreach (IntersectionResult intersectionResult in intersectionResultArray)
+            {
+                XYZ intersectionPoint = intersectionResult.XYZPoint;
+                points.Add(intersectionPoint);
+            }
+
+            return points;
+        }
+
+        /***************************************************/
+
+        [Description("Intersection of curve and curveloop. If there is no intersection, an empty list is returned.")]
+        [Input("curve", "Curve to check the intersection for.")]
+        [Input("curveLoop", "CurveLoop to check the intersection for.")]
+        [Output("points", "List of intersecting points.")]
+        public static List<XYZ> Intersection(this Curve curve, CurveLoop curveLoop)
+        {
+            if (curve == null || curveLoop == null)
+            {
+                return null;
+            }
+
+            List<XYZ> intersectionPoints = new List<XYZ>();
+            foreach (Curve bCurve in curveLoop)
+            {
+                List<XYZ> points = bCurve.Intersection(curve);
+                intersectionPoints.AddRange(points);
+            }
+
+            return intersectionPoints;
+        }
+
+        /***************************************************/
+
+    }
+}
---
+++ Revit_Core_Engine/Query/IsInRange.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+
+using Autodesk.Revit.DB;
+using BH.oM.Base.Attributes;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace BH.Revit.Engine.Core
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /****              Public methods               ****/
+        /***************************************************/
+
+        [Description("Intersection chceck between two bounding boxes.")]
+        [Input("box1", "First bounding box to check the intersection for.")]
+        [Input("box2", "Second bounding box to check the intersection for.")]
+        [Input("tolerance", "Tolerance of interesction checking.")]
+        [Output("bool", "Result of intersection checking. True if bouning boxes are interesecting, false if not.")]
+        public static bool IsInRange(this BoundingBoxXYZ box1, BoundingBoxXYZ box2, double tolerance = BH.oM.Geometry.Tolerance.Distance)
+        {
+            if (box1 == null || box2 == null)
+            {
+                return false;
+            }
+
+            return (box1.Min.X <= box2.Max.X + tolerance && box2.Min.X <= box1.Max.X + tolerance &&
+                     box1.Min.Y <= box2.Max.Y + tolerance && box2.Min.Y <= box1.Max.Y + tolerance &&
+                     box1.Min.Z <= box2.Max.Z + tolerance && box2.Min.Z <= box1.Max.Z + tolerance);
+        }
+    }
+}
