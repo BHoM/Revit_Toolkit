00ebefbf7b0fa56c29999368590f6d0686fb1257
Pawel Baran <pawel.baran@burohappold.com>
2022-08-15 18:24:14 +0200

    IsSimilar method added for surfaces and PanelPlanes tweaked to return only unique planes

--- Revit_Core_Engine/Query/PanelPlanes.cs
+++ Revit_Core_Engine/Query/PanelPlanes.cs
@@ -21,10 +21,13 @@
  */
 
 using Autodesk.Revit.DB;
+using BH.Engine.Adapters.Revit;
+using BH.oM.Adapters.Revit.Settings;
 using BH.oM.Base.Attributes;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 
 namespace BH.Revit.Engine.Core
 {
@@ -36,8 +39,9 @@ namespace BH.Revit.Engine.Core
 
         [Description("Extracts the BHoM-representative (centre, top or bottom, depending on element category) planes from a given Revit host object.")]
         [Input("hostObject", "Revit host object to extract the BHoM-representative planes from.")]
+        [Input("settings", "Revit adapter settings to be used while performing the query.")]
         [Output("planes", "BHoM-representative planes extracted from the input Revit host object.")]
-        public static List<Plane> IPanelPlanes(this HostObject hostObject)
+        public static List<Plane> IPanelPlanes(this HostObject hostObject, RevitSettings settings)
         {
             return PanelPlanes(hostObject as dynamic);
         }
@@ -49,8 +53,9 @@ namespace BH.Revit.Engine.Core
 
         [Description("Extracts the BHoM-representative (centre) planes from a given Revit wall.")]
         [Input("wall", "Revit wall to extract the BHoM-representative planes from.")]
+        [Input("settings", "Revit adapter settings to be used while performing the query.")]
         [Output("planes", "BHoM-representative planes extracted from the input Revit wall.")]
-        public static List<Plane> PanelPlanes(this Wall wall)
+        public static List<Plane> PanelPlanes(this Wall wall, RevitSettings settings)
         {
             Line line = (wall.Location as LocationCurve)?.Curve as Line;
             if (line == null)
@@ -63,16 +68,23 @@ namespace BH.Revit.Engine.Core
 
         [Description("Extracts the BHoM-representative (top) planes from a given Revit floor.")]
         [Input("floor", "Revit floor to extract the BHoM-representative planes from.")]
+        [Input("settings", "Revit adapter settings to be used while performing the query.")]
         [Output("planes", "BHoM-representative planes extracted from the input Revit floor.")]
-        public static List<Plane> PanelPlanes(this Floor floor)
+        public static List<Plane> PanelPlanes(this Floor floor, RevitSettings settings)
         {
-            //TODO: leave unique only!!
+            settings = settings.DefaultIfNull();
+
             List<Plane> result = new List<Plane>();
             foreach (Reference reference in HostObjectUtils.GetTopFaces(floor))
             {
+
                 PlanarFace pf = floor.GetGeometryObjectFromReference(reference) as PlanarFace;
                 if (pf != null)
-                    result.Add(Plane.CreateByNormalAndOrigin(pf.FaceNormal, pf.Origin));
+                {
+                    Plane plane = Plane.CreateByNormalAndOrigin(pf.FaceNormal, pf.Origin);
+                    if (result.All(x => !x.IsSimilar(plane, settings, true)))
+                        result.Add(plane);
+                }
             }
 
             return result;
@@ -82,16 +94,22 @@ namespace BH.Revit.Engine.Core
 
         [Description("Extracts the BHoM-representative (top) planes from a given Revit roof.")]
         [Input("roof", "Revit floor to extract the BHoM-representative planes from.")]
+        [Input("settings", "Revit adapter settings to be used while performing the query.")]
         [Output("planes", "BHoM-representative planes extracted from the input Revit roof.")]
-        public static List<Plane> PanelPlanes(this RoofBase roof)
+        public static List<Plane> PanelPlanes(this RoofBase roof, RevitSettings settings)
         {
-            //TODO: leave unique only!!
+            settings = settings.DefaultIfNull();
+
             List<Plane> result = new List<Plane>();
             foreach (Reference reference in HostObjectUtils.GetTopFaces(roof))
             {
                 PlanarFace pf = roof.GetGeometryObjectFromReference(reference) as PlanarFace;
                 if (pf != null)
-                    result.Add(Plane.CreateByNormalAndOrigin(pf.FaceNormal, pf.Origin));
+                {
+                    Plane plane = Plane.CreateByNormalAndOrigin(pf.FaceNormal, pf.Origin);
+                    if (result.All(x => !x.IsSimilar(plane, settings, true)))
+                        result.Add(plane);
+                }
             }
 
             return result;
@@ -101,16 +119,22 @@ namespace BH.Revit.Engine.Core
 
         [Description("Extracts the BHoM-representative (bottom) planes from a given Revit ceiling.")]
         [Input("ceiling", "Revit ceiling to extract the BHoM-representative planes from.")]
+        [Input("settings", "Revit adapter settings to be used while performing the query.")]
         [Output("planes", "BHoM-representative planes extracted from the input Revit ceiling.")]
-        public static List<Plane> PanelPlanes(this Ceiling ceiling)
+        public static List<Plane> PanelPlanes(this Ceiling ceiling, RevitSettings settings)
         {
-            //TODO: leave unique only!!
+            settings = settings.DefaultIfNull();
+
             List<Plane> result = new List<Plane>();
             foreach (Reference reference in HostObjectUtils.GetBottomFaces(ceiling))
             {
                 PlanarFace pf = ceiling.GetGeometryObjectFromReference(reference) as PlanarFace;
                 if (pf != null)
-                    result.Add(Plane.CreateByNormalAndOrigin(pf.FaceNormal, pf.Origin));
+                {
+                    Plane plane = Plane.CreateByNormalAndOrigin(pf.FaceNormal, pf.Origin);
+                    if (result.All(x => !x.IsSimilar(plane, settings, true)))
+                        result.Add(plane);
+                }
             }
 
             return result;
