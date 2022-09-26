2f26e8865b763d9d970c3580ef4129501acf5e25
Pawel Baran <pawel.baran@burohappold.com>
2022-09-27 00:26:45 +0200

    method enabling filtering visible linked elements added together with context object

---
+++ Revit_Core_Engine/Objects/ActiveViewVisibilityContext.cs
@@ -0,0 +1,223 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using Autodesk.Revit.DB;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace BH.Revit.Engine.Core
+{
+    [Description("Class used to extract elements visible in the active view of the host document. See " + nameof(Query.ElementIdsByVisibleInActiveView) + " for usage example.")]
+    public class ActiveViewVisibilityContext : IExportContext
+    {
+        /***************************************************/
+        /****                Constructor                ****/
+        /***************************************************/
+
+        [Description("hostDocument refers to the document that owns the active view. targetDocument can take three values:" +
+                     "\n- same as hostDocument - visible elements of the host document are then collected" +
+                     "\n- document linked in the host document - elements of that linked document visible in the active view of the host document are then collected" +
+                     "\n- null - all elements of all documents, host and links, are then collected")]
+        public ActiveViewVisibilityContext(Document hostDocument, Document targetDocument)
+        {
+            m_Documents.Push(hostDocument);
+            m_documentLookup[hostDocument.PathName] = hostDocument;
+            m_targetDocument = targetDocument;
+            m_Elements.Add(hostDocument.PathName, new HashSet<ElementId>());
+        }
+
+
+        /***************************************************/
+        /****              Public methods               ****/
+        /***************************************************/
+
+        [Description("Returns ids of all elements visible in the active view of the host document. The ids are grouped by document they belong to.")]
+        public Dictionary<Document, HashSet<ElementId>> GetAllElementsVisibleInActiveView()
+        {
+            if (m_targetDocument != null)
+            {
+                BH.Engine.Base.Compute.RecordError($"The context has been processed with a single target document. Please run again making sure that the target document parameter in the context constructor is null.");
+                return null;
+            }
+            else
+                return m_documentLookup.ToDictionary(x => x.Value, x => m_Elements[x.Key]);
+        }
+
+        /***************************************************/
+
+        [Description("Returns ids of given target document's elements visible in the active view of the host document.")]
+        public HashSet<ElementId> GetElementsVisibleInActiveView(Document targetDocument)
+        {
+            HashSet<ElementId> result = null;
+            if (!m_Elements.TryGetValue(targetDocument?.PathName, out result))
+                BH.Engine.Base.Compute.RecordError($"Document {targetDocument.Title} has not been processed in the context. Please run again making sure the document is passed as target document in the context constructor or the target document parameter is null.");
+
+            return result;
+        }
+
+        /***************************************************/
+
+        public void Finish()
+        {
+        }
+
+        /***************************************************/
+
+        public bool IsCanceled()
+        {
+            return false;
+        }
+
+        /***************************************************/
+
+        public RenderNodeAction OnElementBegin(ElementId elementId)
+        {
+            Document doc = m_Documents.Peek();
+            m_Elements[doc.PathName].Add(elementId);
+
+            Element element = doc.GetElement(elementId);
+            if (element is RevitLinkInstance)
+            {
+                RevitLinkInstance linkInstance = (RevitLinkInstance)element;
+                if (m_targetDocument == null || m_targetDocument.PathName == linkInstance.GetLinkDocument().PathName)
+                {
+                    m_documentLookup[linkInstance.Document.PathName] = linkInstance.Document;
+                    return RenderNodeAction.Proceed;
+                }
+            }
+
+            return RenderNodeAction.Skip;
+        }
+
+        /***************************************************/
+
+        public void OnElementEnd(ElementId elementId)
+        {
+        }
+
+        /***************************************************/
+
+        public RenderNodeAction OnFaceBegin(FaceNode node)
+        {
+            return RenderNodeAction.Skip;
+        }
+
+        /***************************************************/
+
+        public void OnFaceEnd(FaceNode node)
+        {
+        }
+
+        /***************************************************/
+
+        public RenderNodeAction OnInstanceBegin(InstanceNode node)
+        {
+            return RenderNodeAction.Skip;
+        }
+
+        /***************************************************/
+
+        public void OnInstanceEnd(InstanceNode node)
+        {
+        }
+
+        /***************************************************/
+
+        /***************************************************/
+
+        public void OnLight(LightNode node)
+        {
+        }
+
+        /***************************************************/
+
+        public RenderNodeAction OnLinkBegin(LinkNode node)
+        {
+            Document doc = node.GetDocument();
+            m_Documents.Push(doc);
+            if (!m_Elements.ContainsKey(doc.PathName))
+                m_Elements[doc.PathName] = new HashSet<ElementId>();
+
+            return RenderNodeAction.Proceed;
+        }
+
+        /***************************************************/
+
+        public void OnLinkEnd(LinkNode node)
+        {
+            m_Documents.Pop();
+        }
+
+        /***************************************************/
+
+        public void OnMaterial(MaterialNode node)
+        {
+        }
+
+        /***************************************************/
+
+        public void OnPolymesh(PolymeshTopology node)
+        {
+        }
+
+        /***************************************************/
+
+        /***************************************************/
+
+        public void OnRPC(RPCNode node)
+        {
+        }
+
+        /***************************************************/
+
+        public RenderNodeAction OnViewBegin(ViewNode node)
+        {
+            return RenderNodeAction.Proceed;
+        }
+
+        /***************************************************/
+
+        public void OnViewEnd(ElementId elementId)
+        {
+        }
+
+        /***************************************************/
+
+        public bool Start()
+        {
+            return true;
+        }
+
+
+        /***************************************************/
+        /****              Private fields               ****/
+        /***************************************************/
+
+        private Document m_targetDocument;
+        private Stack<Document> m_Documents = new Stack<Document>();
+        private Dictionary<string, Document> m_documentLookup = new Dictionary<string, Document>();
+        private Dictionary<string, HashSet<ElementId>> m_Elements = new Dictionary<string, HashSet<ElementId>>();
+
+        /***************************************************/
+    }
+}
---
+++ Revit_Core_Engine/Query/ElementIds/ElementIdsByVisibleInActiveView.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using Autodesk.Revit.DB;
+using BH.oM.Base.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace BH.Revit.Engine.Core
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /****              Public methods               ****/
+        /***************************************************/
+
+        [Description("Filters ElementIds of elements that are visible in active view of the host document. If the input document is a linked document, its elements visible in the active view of the host document will be returned.")]
+        [Input("document", "Revit document to be processed (can be a linked document).")]
+        [Input("ids", "Optional, allows narrowing the search: if not null, the output will be an intersection of this collection and ElementIds filtered by the query.")]
+        [Output("elementIds", "Collection of filtered ElementIds.")]
+        public static IEnumerable<ElementId> ElementIdsByVisibleInActiveView(this Document document, IEnumerable<ElementId> ids = null)
+        {
+            if (document == null)
+                return null;
+
+            Document hostDocument = document.IsLinked ? document.HostDocument() : document;
+            ActiveViewVisibilityContext context = new ActiveViewVisibilityContext(hostDocument, document);
+            CustomExporter exporter = new CustomExporter(hostDocument, context);
+            exporter.IncludeGeometricObjects = false;
+            exporter.ShouldStopOnError = false;
+
+            try
+            {
+                exporter.Export(hostDocument.ActiveView);
+            }
+            catch (Exception ex)
+            {
+                BH.Engine.Base.Compute.RecordError("Visible elements could not be queried from the active view because it is not queryable (it is drafting, template etc.).");
+                return null;
+            }
+
+            HashSet<ElementId> result = context.GetElementsVisibleInActiveView(document);
+            if (ids != null)
+                result.IntersectWith(ids);
+
+            return result;
+        }
+
+        /***************************************************/
+    }
+
+   
+}
