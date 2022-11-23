6f7aae47afae9ebaad896123d60e4a71ad6f3efa
Isak Naslund <Isak.Naslund@burohappold.com>
2022-11-23 10:24:23 +0100

    Respond to comments

--- Revit_Core_Engine/Convert/Physical/FromRevit/MeshElement.cs
+++ Revit_Core_Engine/Convert/Physical/FromRevit/MeshElement.cs
@@ -41,27 +41,26 @@ namespace BH.Revit.Engine.Core
         /****               Public Methods              ****/
         /***************************************************/
 
-        [Description("Converts a Revit FamilyInstance to BH.oM.Physical.Elements.Beam.")]
-        [Input("familyInstance", "Revit FamilyInstance to be converted.")]
+        [Description("Converts a Revit Element to BHoM.Physical.Elements.MeshElement.")]
+        [Input("Element", "Revit Element to be converted.")]
         [Input("settings", "Revit adapter settings to be used while performing the convert.")]
         [Input("refObjects", "Optional, a collection of objects already processed in the current adapter action, stored to avoid processing the same object more than once.")]
-        [Output("beam", "BH.oM.Physical.Elements.Beam resulting from converting the input Revit FamilyInstance.")]
+        [Output("meshElement", "BH.oM.Physical.Elements.MeshElement resulting from converting the input Revit Element.")]
         public static MeshElement MeshElementFromRevit(this Element element, RevitSettings settings = null, Dictionary<string, List<IBHoMObject>> refObjects = null)
         {
             settings = settings.DefaultIfNull();
 
-            MeshElement meshElement = refObjects.GetValue<MeshElement>(element.Id);
+            MeshElement meshElement = refObjects?.GetValue<MeshElement>(element.Id);
             if (meshElement != null)
                 return meshElement;
 
-            Options meshOptions = Create.Options(ViewDetailLevel.Medium, true, true);
+            Options meshOptions = Create.Options(ViewDetailLevel.Medium, false, false);
 
-            meshElement = new MeshElement();
-
-            List<oM.Geometry.Mesh> meshes = element.MeshedGeometry(meshOptions, settings);
-
-            if (meshes != null && meshes.Any())
-                meshElement.Mesh = meshes.Join(false);
+            meshElement = new MeshElement()
+            {
+                Name = element.Name,
+                Mesh = element.MeshedGeometry(meshOptions, settings)?.Join(false)
+            };
 
             //Set identifiers, parameters & custom data
             meshElement.SetIdentifiers(element);
@@ -69,12 +68,11 @@ namespace BH.Revit.Engine.Core
             meshElement.SetProperties(element, settings.MappingSettings);
 
             refObjects.AddOrReplace(element.Id, meshElement);
+
             return meshElement;
         }
 
         /***************************************************/
-
-
     }
 }
 
