daf0093ccccf60735d157094ffcfa4ff1b4238bf
BHoMBot <BHoMBot@burohappold.com>
2023-01-03 11:45:56 +0000

    Upgrade AssemblyFileVersion to 6.1.0.0

--- Revit_Core_Engine/Properties/AssemblyInfo.cs
+++ Revit_Core_Engine/Properties/AssemblyInfo.cs
@@ -55,7 +55,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("6.0.0.0")]
-[assembly: AssemblyFileVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.1.0.0")]
 
 
 
