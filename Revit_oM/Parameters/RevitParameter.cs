e846aa873ea23ba9ddf35e55f1a94b26d967526e
Pawel Baran <pawel.baran@burohappold.com>
2023-03-01 01:00:49 +0100

    unit type property added to RevitParameter

--- Revit_oM/Parameters/RevitParameter.cs
+++ Revit_oM/Parameters/RevitParameter.cs
@@ -40,6 +40,8 @@ namespace BH.oM.Adapters.Revit.Parameters
         [Description("Value of the Revit parameter. Enums are converted to strings, ElementIds to integers.")]
         public virtual object Value { get; set; } = null;
 
+        public virtual string UnitType { get; set; }
+
         [Description("Whether the parameter is read only or modifiable by the Revit user.")]
         public virtual bool IsReadOnly { get; set; } = false;
 
