9ffb07803058867d4e82f7237f0cf282454c56ad
Pawel Baran <pawel.baran@burohappold.com>
2023-04-21 00:32:51 +0200

    1st iteration of new, better category set

---
+++ Revit_oM/Attributes/CategoryTypeAttribute.cs
@@ -0,0 +1,27 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Revit.Attributes
+{
+    public class CategoryTypeAttribute : Attribute, IImmutable, IObject
+    {
+        private readonly string m_Type;
+
+        public string Type
+        {
+            get
+            {
+                return m_Type;
+            }
+        }
+
+        public CategoryTypeAttribute(string type)
+        {
+            m_Type = type;
+        }
+    }
+}
