140b3ceb320bd7801ddbe94cce56238de15ce00b
Michal Pekacki <michal.pekacki@burohappold.com>
2023-05-19 16:05:09 +0200

    added 3DView CropBox alignment

---
+++ Revit_Core_Engine/Query/Points.cs
@@ -0,0 +1,61 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using Autodesk.Revit.DB;
+using BH.oM.Base.Attributes;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace BH.Revit.Engine.Core
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /****              Public methods               ****/
+        /***************************************************/
+
+        [Description("Queries edge points of BoundingBoxXYZ.")]
+        [Input("bbox", "BoundingBoxXYZ to get the points from.")]
+        [Output("edgeCurves", "Edge points of the BoundingBoxXYZ.")]
+        public static List<XYZ> Points(this BoundingBoxXYZ bbox)
+        {
+            if (bbox == null)
+                return null;
+
+            var bboxPoints = new List<XYZ>();
+
+            bboxPoints.Add(new XYZ(bbox.Min.X, bbox.Min.Y, bbox.Min.Z));
+            bboxPoints.Add(new XYZ(bbox.Max.X, bbox.Min.Y, bbox.Min.Z));
+            bboxPoints.Add(new XYZ(bbox.Min.X, bbox.Max.Y, bbox.Min.Z));
+            bboxPoints.Add(new XYZ(bbox.Max.X, bbox.Max.Y, bbox.Min.Z));
+            bboxPoints.Add(new XYZ(bbox.Min.X, bbox.Min.Y, bbox.Max.Z));
+            bboxPoints.Add(new XYZ(bbox.Max.X, bbox.Min.Y, bbox.Max.Z));
+            bboxPoints.Add(new XYZ(bbox.Min.X, bbox.Max.Y, bbox.Max.Z));
+            bboxPoints.Add(new XYZ(bbox.Max.X, bbox.Max.Y, bbox.Max.Z));
+
+            return bboxPoints;
+        }
+
+        /***************************************************/
+    }
+}
+
