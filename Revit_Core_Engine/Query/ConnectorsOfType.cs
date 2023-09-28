6fa527822868abb7e7f42b48c3620195f4c16ca5
Pawel Baran <pawel.baran@burohappold.com>
2023-09-28 17:53:27 +0200

    MEP element orientation issues fixed

---
+++ Revit_Core_Engine/Query/ConnectorsOfType.cs
@@ -0,0 +1,20 @@
+﻿using Autodesk.Revit.DB;
+using System.Collections.Generic;
+
+namespace BH.Revit.Engine.Core
+{
+    public static partial class Query
+    {
+        public static List<Connector> ConnectorsOfType(this MEPCurve mepCurve, ConnectorType connectorType)
+        {
+            List<Connector> result = new List<Connector>();
+            foreach (Connector connector in mepCurve.ConnectorManager.Connectors)
+            {
+                if (connector.ConnectorType == connectorType)
+                    result.Add(connector);
+            }
+
+            return result;
+        }
+    }
+}
