73d4b18efd2465186161cf3b1e398a9544a0e9d4
Pawel Baran <pawel.baran@burohappold.com>
2023-12-27 13:17:08 +0100

    code tweaked to support Revit 2024

---
+++ Revit_Core_Engine/Query/Symbol.cs
@@ -0,0 +1,30 @@
+﻿using Autodesk.Revit.DB;
+using BH.oM.Base.Attributes;
+using System.ComponentModel;
+
+namespace BH.Revit.Engine.Core
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /****               Public Methods              ****/
+        /***************************************************/
+
+        [Description("Returns symbol element of a given GeometryInstance, e.g. mullion type for a geometry instance representing a mullion.")]
+        [Input("geometryInstance", "GeometryInstance to find symbol element for.")]
+        [Output("symbol", "Symbol element of the input GeometryInstance.")]
+        public static Element Symbol(this GeometryInstance geometryInstance)
+        {
+            if (geometryInstance == null)
+                return null;
+
+#if (REVIT2020 || REVIT2021 || REVIT2022)
+            return geometryInstance.Symbol;
+#else
+            return geometryInstance.GetDocument().GetElement(geometryInstance.GetSymbolGeometryId().SymbolId);
+#endif
+        }
+
+        /***************************************************/
+    }
+}
