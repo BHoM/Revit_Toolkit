7abafcc3a998ccf22f6aae0743073599e9d3c87d
Pawel Baran <pawel.baran@burohappold.com>
2024-01-29 19:04:11 +0100

    draft oM classes added

---
+++ Revit_oM/Elements/ViewFilter.cs
@@ -0,0 +1,16 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Adapters.Revit.Elements
+{
+    public class ViewFilter : BHoMObject
+    {
+        public virtual List<string> Categories {  get; set; }
+
+        //public List<FilterRule> Rules { get; set; }
+    }
+}
