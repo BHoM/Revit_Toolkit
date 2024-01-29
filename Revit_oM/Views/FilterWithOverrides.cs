6525ab701deb0f62401386571c46cf2e2599ae60
Pawel Baran <pawel.baran@burohappold.com>
2024-01-29 19:04:11 +0100

    draft oM classes added

---
+++ Revit_oM/Views/FilterWithOverrides.cs
@@ -0,0 +1,17 @@
+﻿using BH.oM.Adapters.Revit.Elements;
+using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Adapters.Revit
+{
+    public class FilterWithOverrides
+    {
+        public virtual ViewFilter Filter { get; set; }
+
+        //public virtual List<Override> Overrides { get; set; }
+    }
+}
