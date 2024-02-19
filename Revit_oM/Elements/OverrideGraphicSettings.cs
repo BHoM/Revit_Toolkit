5fe79b2591f412c816d8c5fb1ba16a3d7e32d80f
GCRA101 <giorgio.albieri@burohappold.com>
2024-02-19 07:27:10 +0000

    Add bespoke classes defining graphics settings for new view filters 	- Enum LinePattern 	- Enum SolidPattern 	- Class OverrideGraphicSettings 	- Class FilterRule

---
+++ Revit_oM/Elements/OverrideGraphicSettings.cs
@@ -0,0 +1,21 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Revit.Elements
+{
+    public class OverrideGraphicSettings : BHoMObject
+    {   
+        public virtual Color LineColor {  get; set; }
+        public virtual Color CutColor {  get; set; }
+        public virtual Color SurfaceColor {  get; set; }
+        public virtual LinePattern LinePattern {  get; set; }
+        public virtual FillPattern CutPattern {  get; set; }
+        public virtual FillPattern SurfacePattern {  get; set; }
+
+    }
+}
