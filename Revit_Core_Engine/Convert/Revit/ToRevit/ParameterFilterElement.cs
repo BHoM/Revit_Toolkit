970b4f2facd25fc2fa6ae8e1ffe5e03a045a5e08
GCRA101 <giorgio.albieri@burohappold.com>
2024-02-20 21:11:25 +0000

    Implement Conversion of BHoM classes to Revit

--- Revit_Core_Engine/Convert/Revit/ToRevit/ParameterFilterElement.cs
+++ Revit_Core_Engine/Convert/Revit/ToRevit/ParameterFilterElement.cs
@@ -20,15 +20,21 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
+using Autodesk.Revit.Creation;
 using Autodesk.Revit.DB;
 using BH.oM.Adapters.Revit.Settings;
 using BH.oM.Base;
 using BH.oM.Base.Attributes;
+using BH.oM.MEP.Equipment.Parts;
 using BH.oM.Physical.Elements;
+using BH.oM.Revit.Views;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using Document = Autodesk.Revit.DB.Document;
+using FilterRule = BH.oM.Revit.Views.FilterRule;
 
 namespace BH.Revit.Engine.Core
 {
@@ -38,22 +44,48 @@ namespace BH.Revit.Engine.Core
         /****               Public Methods              ****/
         /***************************************************/
 
-        //[Description("Converts BH.oM.Adapters.Revit.Elements.Assembly to a Revit AssemblyInstance." +
-        //             "\nOnly the assembly instance itself is created, while any updates of name or parameters need to happen in a separate transaction, which is caused by Revit API limitations.")]
-        //[Input("assembly", "BH.oM.Adapters.Revit.Elements.Assembly to be converted.")]
-        //[Input("document", "Revit document, in which the output of the convert will be created.")]
-        //[Input("settings", "Revit adapter settings to be used while performing the convert.")]
-        //[Input("refObjects", "Optional, a collection of objects already processed in the current adapter action, stored to avoid processing the same object more than once.")]
-        //[Output("assemblyInstance", "Revit AssemblyInstance resulting from converting the input BH.oM.Adapters.Revit.Elements.Assembly.")]
+        [Description("Converts BH.oM.Adapters.Revit.Elements.ViewFilter to a Revit ParameterFilterElement." +
+                     "\nThe Filter Rules are assigned after its creation due to Revit API limitations.")]
+        [Input("filter", "BH.oM.Adapters.Revit.Elements.ViewFilter to be converted.")]
+        [Input("document", "Revit document, in which the output of the convert will be created.")]
+        [Input("settings", "Revit adapter settings to be used while performing the convert.")]
+        [Input("refObjects", "Optional, a collection of objects already processed in the current adapter action, stored to avoid processing the same object more than once.")]
+        [Output("filter", "Revit ParameterFilterElement resulting from converting the input BH.oM.Adapters.Revit.Elements.ViewFilter.")]
         public static ParameterFilterElement ToRevitParameterFilterElement(this oM.Adapters.Revit.Elements.ViewFilter filter, Document document, RevitSettings settings = null, Dictionary<Guid, List<int>> refObjects = null)
         {
             ParameterFilterElement revitFilter = refObjects.GetValue<ParameterFilterElement>(document, filter.BHoM_Guid);
             if (revitFilter != null)
                 return revitFilter;
 
-            //TODO: fill this in with the logic
-            List<ElementId> categories = new List<ElementId>();
-            revitFilter = ParameterFilterElement.Create(document, filter.Name, categories);
+            // 1. CREATE PARAMETERFILTERELEMENT with ASSIGNED CATEGORIES LIST
+
+            /* 1.1 Turn list of Category Names into List of Category ElementIds via use of Streams */
+
+            List<ElementId> categoryIdsList = filter.Categories
+                             // Format the string name of the categories
+                             .Select(catName => catName.ToUpper().Replace(" ", ""))
+                             // Get the corresponding BuiltInCategories
+                             .Select(catName => { List<string> builtInCatNames = Enum.GetNames(typeof(BuiltInCategory))
+                                                        .Select(builtInCategoryName => builtInCategoryName.ToUpper().Replace("ost_", ""))
+                                                        .ToList();
+                                                  return (BuiltInCategory)(((BuiltInCategory[])Enum.GetValues(typeof(BuiltInCategory)))[builtInCatNames.IndexOf(catName)]);})
+                             // Get the ElementIds of the BuiltInCategories
+                             .Select(builtInCat => new ElementId(builtInCat))
+                             // Turn the Stream into a List of ElementIds
+                             .ToList<ElementId>();
+            
+            /* 1.2 Create the ParameterFilterElement in the current Revit Document */
+            revitFilter = ParameterFilterElement.Create(document, filter.Name, categoryIdsList);
+
+
+            // 2. BUILD THE REVIT FILTER RULES and ASSIGN THEM TO THE PARAMETERFILTERELEMENT
+
+            /* Via use of Streams*/
+            revitFilter.SetElementFilter(new LogicalAndFilter(filter.Rules
+                            .Select(filterRule => filterRuleToRevit(document, filterRule))
+                            .Select(revitFilterRule => new ElementParameterFilter(revitFilterRule))
+                            .Cast<ElementFilter>()
+                            .ToList()));
 
             revitFilter.CopyParameters(filter, settings);
             refObjects.AddOrReplace(filter, revitFilter);
@@ -61,6 +93,75 @@ namespace BH.Revit.Engine.Core
         }
 
         /***************************************************/
+
+        private static Autodesk.Revit.DB.FilterRule filterRuleToRevit(Document document,FilterRule filterRule) {
+
+            Autodesk.Revit.DB.FilterRule revitFilterRule = null;
+
+            ElementClassFilter paramFilter = new ElementClassFilter(typeof(Parameter));
+            ElementClassFilter builtInParamFilter = new ElementClassFilter(typeof(BuiltInParameter));
+            LogicalOrFilter logicalOrFilter = new LogicalOrFilter(paramFilter, builtInParamFilter);
+
+
+            ElementId parameterId = new FilteredElementCollector(document)
+                            .WherePasses(logicalOrFilter)
+                            .Where(par => par.Name.Equals(filterRule.ParameterName))
+                            .First()
+                            .Id;
+
+            switch (filterRule.RuleType) { 
+                case FilterRuleType.BEGINSWITH:
+                    revitFilterRule=ParameterFilterRuleFactory
+                        .CreateBeginsWithRule(parameterId, (string) filterRule.Value, false);
+                    break;
+                case FilterRuleType.CONTAINS:
+                    revitFilterRule = ParameterFilterRuleFactory
+                        .CreateContainsRule(parameterId, (string) filterRule.Value, false);
+                    break;
+                case FilterRuleType.ENDSWITH:
+                    revitFilterRule = ParameterFilterRuleFactory
+                        .CreateEndsWithRule(parameterId, (string) filterRule.Value, false);
+                    break;
+                case FilterRuleType.EQUALS:
+                    revitFilterRule = ParameterFilterRuleFactory
+                        .CreateEqualsRule(parameterId, (int) filterRule.Value);
+                    break;
+                case FilterRuleType.GREATER:
+                    revitFilterRule = ParameterFilterRuleFactory
+                        .CreateGreaterRule(parameterId, (ElementId) filterRule.Value);
+                    break;
+                case FilterRuleType.GREATER_OR_EQUAL:
+                    revitFilterRule = ParameterFilterRuleFactory
+                        .CreateGreaterOrEqualRule(parameterId, (ElementId) filterRule.Value);
+                    break;
+                case FilterRuleType.LESS:
+                    revitFilterRule = ParameterFilterRuleFactory
+                        .CreateLessRule(parameterId, (ElementId) filterRule.Value);
+                    break;
+                case FilterRuleType.LESS_OR_EQUAL:
+                    revitFilterRule = ParameterFilterRuleFactory
+                        .CreateLessOrEqualRule(parameterId, (ElementId) filterRule.Value);
+                    break;
+                case FilterRuleType.NOT_BEGINSWITH:
+                    revitFilterRule = ParameterFilterRuleFactory
+                        .CreateNotBeginsWithRule(parameterId, (string) filterRule.Value, false);
+                    break;
+                case FilterRuleType.NOT_CONTAINS:
+                    revitFilterRule = ParameterFilterRuleFactory
+                        .CreateNotContainsRule(parameterId, (string) filterRule.Value, false);
+                    break;
+                case FilterRuleType.NOT_ENDSWITH:
+                    revitFilterRule = ParameterFilterRuleFactory
+                        .CreateNotEndsWithRule(parameterId, (string) filterRule.Value, false);
+                    break;
+                default:
+                    break;}
+
+            return revitFilterRule;
+        }
     }
+
+
+
 }
 
