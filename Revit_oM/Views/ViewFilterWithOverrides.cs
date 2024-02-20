b38eab0535a35ad44da66ea0106d84991c24c4cb
GCRA101 <giorgio.albieri@burohappold.com>
2024-02-20 21:14:29 +0000

    Add minor updates to BHoM ViewFilterOverrides Class

--- Revit_oM/Views/ViewFilterWithOverrides.cs
+++ Revit_oM/Views/ViewFilterWithOverrides.cs
@@ -1,17 +1,16 @@
 ﻿using BH.oM.Adapters.Revit.Elements;
 using BH.oM.Base;
-using BH.oM.Revit.Elements;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BH.oM.Adapters.Revit
+namespace BH.oM.Revit.Views
 {
-    public class ViewFilterWithOverrides
+    public class ViewFilterWithOverrides: BHoMObject
     {
         public virtual ViewFilter Filter { get; set; }
-        public virtual List<OverrideGraphicSettings> Overrides { get; set; }
+        public virtual OverrideGraphicSettings Overrides { get; set; }
     }
 }
