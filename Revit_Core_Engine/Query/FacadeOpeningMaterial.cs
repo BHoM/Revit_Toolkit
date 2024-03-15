8ee47265d0d9ceca59493005d438fd52e8d80891
Pawel Baran <pawel.baran@burohappold.com>
2024-03-15 20:56:47 +0100

    facade-related queries cleaned up and extended

---
+++ Revit_Core_Engine/Query/FacadeOpeningMaterial.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2024, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using Autodesk.Revit.DB;
+using System.Collections.Generic;
+
+namespace BH.Revit.Engine.Core
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /****              Public methods               ****/
+        /***************************************************/
+
+        public static Material FacadeOpeningMaterial(this FamilyInstance opening)
+        {
+            List<Solid> solids = opening?.Solids(new Options());
+            if (solids == null)
+                return null;
+
+            Face maxAreaFace = null;
+            foreach (Solid solid in solids)
+            {
+                foreach (Face face in solid.Faces)
+                {
+                    if (maxAreaFace == null || maxAreaFace.Area < face.Area)
+                        maxAreaFace = face;
+                }
+            }
+
+            return maxAreaFace != null ? opening.Document.GetElement(maxAreaFace.MaterialElementId) as Material : null;
+        }
+
+        /***************************************************/
+    }
+}
