0c1c4bf8f0d172a87150a140adc222e545e0ef58
GCRA101 <giorgio.albieri@burohappold.com>
2024-04-28 19:29:58 +0100

    Add ParameterValuePresenceRule
    Add it as BHoM Class and implement code for conversion to/from Revit

---
+++ Revit_oM/Elements/ParameterValuePresenceRule.cs
@@ -0,0 +1,14 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Adapters.Revit.Elements
+{
+    public class ParameterValuePresenceRule : FilterValueRule
+    {
+        public virtual bool IsPresent {  get; set; }
+    }
+}
