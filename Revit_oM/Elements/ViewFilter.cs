b3484150cf4011905082fdae15f0fa31b269093c
GCRA101 <giorgio.albieri@burohappold.com>
2024-10-15 11:36:50 +0100

    Incorporate PR comments from Pawel Baran - Part02

--- Revit_oM/Elements/ViewFilter.cs
+++ Revit_oM/Elements/ViewFilter.cs
@@ -6,12 +6,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BH.oM.Revit.Enums;
 
 namespace BH.oM.Adapters.Revit.Elements
 {
     public class ViewFilter : BHoMObject
     {
-        public virtual List<string> Categories {  get; set; }
+        public virtual List<Category> Categories {  get; set; }
         public virtual List<FilterRule> Rules { get; set; }
     }
 }
