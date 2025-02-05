3d87f48658639b3e8d3a99c59d6c9e654a99f01b
linhnam-nguyen <88286063+linhnam-nguyen@users.noreply.github.com>
2025-02-05 17:17:40 +0100

    Add Filters on Pulled objects to reduce request computational needs

---
+++ Revit_Engine/Query/FilterUniqueRevitElements.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2025, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Adapters.Revit;
+using BH.oM.Base;
+using BH.oM.Base.Attributes;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace BH.Engine.Adapters.Revit
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /****              Public methods               ****/
+        /***************************************************/
+
+        [Description("Filters the given collection of BHoM objects to return only unique Revit elements based on their UniqueId.")]
+        [Input("bHoMObjects", "The collection of BHoM objects to filter.")]
+        [Output("A list of unique BHoM objects.")]
+        public static List<IBHoMObject> FilterUniqueRevitElements(this IEnumerable<IBHoMObject> bHoMObjects)
+        {						
+            return bHoMObjects
+                .GroupBy(x => x.UniqueId())
+                .Select(g => g.First())
+                .ToList();
+        }
+
+        /***************************************************/
+    }
+}
+
+
+
+
+
+
+
+
