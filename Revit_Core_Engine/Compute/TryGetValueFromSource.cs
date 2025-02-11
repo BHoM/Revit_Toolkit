47c4cf8121068c647ce5eabc4d3f90d051e633d8
Pawel Baran <pawel.baran@burohappold.com>
2025-02-11 13:36:57 +0100

    compliance fix

--- Revit_Core_Engine/Compute/TryGetValueFromSource.cs
+++ Revit_Core_Engine/Compute/TryGetValueFromSource.cs
@@ -34,8 +34,8 @@ namespace BH.Revit.Engine.Core
         /****              Public Methods               ****/
         /***************************************************/
 
-        [Description("Tries to extract a value from an object based on the instruction embedded in the provided " + nameof(ParameterValueSource) + ".")]
-        [Input("obj", "Object to extract the value from.")]
+        [Description("Tries to extract a value from an element based on the instruction embedded in the provided " + nameof(ParameterValueSource) + ".")]
+        [Input("element", "Element to extract the value from.")]
         [Input("valueSource", "Object defining how to extract the value from the input object.")]
         [MultiOutput(0, "found", "True if value source exists in the input object (i.e. value could be extracted from the object), otherwise false.")]
         [MultiOutput(1, "value", "Value extracted from the input object based on the provided instruction.")]
