547908b844f64417caf59e8d1077f849a543df0a
Pawel Baran <pawel.baran@burohappold.com>
2025-03-19 10:09:55 +0100

    descriptions and copyright added

--- Revit_Core_Engine/Query/IsEnumParameter.cs
+++ Revit_Core_Engine/Query/IsEnumParameter.cs
@@ -1,14 +1,40 @@
-﻿using Autodesk.Revit.DB;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2025, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using Autodesk.Revit.DB;
+using BH.oM.Base.Attributes;
+using System.ComponentModel;
 
 namespace BH.Revit.Engine.Core
 {
     public static partial class Query
     {
+        /***************************************************/
+        /****              Public methods               ****/
+        /***************************************************/
+
+        [Description("Checks whether the Revit parameter is an enum (dropdown in Revit).")]
+        [Input("parameter", "Revit parameter to check whether it is an enum.")]
+        [Output("enum", "True if the Revit parameter is an enum, otherwise false.")]
         public static bool IsEnumParameter(this Parameter parameter)
         {
             if (parameter?.StorageType != StorageType.Integer)
@@ -21,5 +47,7 @@ namespace BH.Revit.Engine.Core
             return dataType == null;
 #endif
         }
+
+        /***************************************************/
     }
 }
