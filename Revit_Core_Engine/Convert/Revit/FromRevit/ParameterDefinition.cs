c3913fc65595f9b2bd5fc38d8798844cd8170fd3
linhnam-nguyen <88286063+linhnam-nguyen@users.noreply.github.com>
2025-04-10 16:25:10 +0200

    Create pulled ParameterDefinition

---
+++ Revit_Core_Engine/Convert/Revit/FromRevit/ParameterDefinition.cs
@@ -0,0 +1,81 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2025, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using Autodesk.Revit.DB;
+using BH.Engine.Adapters.Revit;
+using BH.oM.Adapters.Revit.Mapping;
+using BH.oM.Adapters.Revit.Parameters;
+using BH.oM.Adapters.Revit.Settings;
+using BH.oM.Base;
+using BH.oM.Base.Attributes;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+
+namespace BH.Revit.Engine.Core
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /****               Public Methods              ****/
+        /***************************************************/
+
+
+        [Description("Converts a ParameterElement to BH.oM.Adapters.Revit.Parameters.ParameterDefinition.")]
+        [Input("parameter", "Revit Parameter to be converted.")]
+        [Input("parameterLinks", "A collection of names of RevitParameters and sets of their correspondent Revit parameter names to be used on name mapping.")]
+        [Input("onlyLinked", "If true, there needs to be a valid, relevant parameter link in parameterLinks in order for convert to succeed.")]
+        [Output("parameterDefinition", "BH.oM.Adapters.Revit.Parameters.ParameterDefinition resulting from converting the input Revit Parameter.")]
+        public static ParameterDefinition ParameterDefinitionFromRevit(this ParameterElement parameter,Document document, RevitSettings settings = null, Dictionary<string, List<IBHoMObject>> refObjects = null)
+        {
+            settings = settings.DefaultIfNull();
+            ParameterDefinition paramDef = refObjects.GetValue<ParameterDefinition>(parameter.Id);
+            if (paramDef != null)
+                return paramDef;
+
+            Definition def = parameter.GetDefinition();
+            ElementBinding binding = document.ParameterBindings.get_Item(def) as ElementBinding;
+
+            string name = parameter.Name;
+            string parameterType = def.GetDataType().SpecName();
+            string discipline = def.GetDataType().UnitName();
+            string parameterGroup = def.GetDataType().GroupName();
+            bool instance = binding is InstanceBinding;
+            List<string> categories = binding.Categories.Cast<Category>().Select(c => c.Name).ToList() ;
+            bool shared = true;
+
+            paramDef = new ParameterDefinition
+            {
+                Name = name,
+                ParameterType = parameterType,
+                Discipline = discipline,
+                Instance = instance,
+                Categories = categories,
+                Shared = shared
+            };
+
+            return paramDef;
+        }
+
+        /***************************************************/
+    }
+}
