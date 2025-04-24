59506795259480dec5206131c894e5ec3a8a8dd1
linhnam-nguyen <88286063+linhnam-nguyen@users.noreply.github.com>
2025-04-24 10:50:30 +0200

    Minor adjustments

--- Revit_Core_Engine/Convert/Revit/FromRevit/ParameterDefinition.cs
+++ Revit_Core_Engine/Convert/Revit/FromRevit/ParameterDefinition.cs
@@ -33,71 +33,71 @@ using BH.oM.Adapters.Revit.Elements;
 
 namespace BH.Revit.Engine.Core
 {
-    public static partial class Convert
-    {
-        /***************************************************/
-        /****               Public Methods              ****/
-        /***************************************************/
+	public static partial class Convert
+	{
+		/***************************************************/
+		/****               Public Methods              ****/
+		/***************************************************/
 
 
-        [Description("Converts a ParameterElement to BH.oM.Adapters.Revit.Parameters.ParameterDefinition.")]
-        [Input("parameter", "Revit Parameter to be converted.")]
-        [Input("parameterLinks", "A collection of names of RevitParameters and sets of their correspondent Revit parameter names to be used on name mapping.")]
-        [Input("onlyLinked", "If true, there needs to be a valid, relevant parameter link in parameterLinks in order for convert to succeed.")]
-        [Output("parameterDefinition", "BH.oM.Adapters.Revit.Parameters.ParameterDefinition resulting from converting the input Revit Parameter.")]
-        public static ParameterDefinition ParameterElementFromRevit(this ParameterElement parameter, RevitSettings settings = null, Dictionary<string, List<IBHoMObject>> refObjects = null)
-        {
-            settings = settings.DefaultIfNull();
-            ParameterDefinition paramDef = refObjects.GetValue<ParameterDefinition>(parameter.Id);
-            if (paramDef != null)
-                return paramDef;
+		[Description("Converts a ParameterElement to BH.oM.Adapters.Revit.Parameters.ParameterDefinition.")]
+		[Input("parameter", "Revit Parameter to be converted.")]
+		[Input("parameterLinks", "A collection of names of RevitParameters and sets of their correspondent Revit parameter names to be used on name mapping.")]
+		[Input("onlyLinked", "If true, there needs to be a valid, relevant parameter link in parameterLinks in order for convert to succeed.")]
+		[Output("parameterDefinition", "BH.oM.Adapters.Revit.Parameters.ParameterDefinition resulting from converting the input Revit Parameter.")]
+		public static ParameterDefinition ParameterElementFromRevit(this ParameterElement parameter, RevitSettings settings = null, Dictionary<string, List<IBHoMObject>> refObjects = null)
+		{
+			settings = settings.DefaultIfNull();
+			ParameterDefinition paramDefinition = refObjects.GetValue<ParameterDefinition>(parameter.Id);
+			if (paramDefinition != null)
+				return paramDefinition;
 
-            Definition def = parameter.GetDefinition();
-            Document document = parameter.Document;
-            Binding binding = document.ParameterBindings.get_Item(def);
-            ElementBinding elementBinding = binding as ElementBinding;
+			Definition def = parameter.GetDefinition();
+			Document document = parameter.Document;
+			Binding binding = document.ParameterBindings.get_Item(def);
+			ElementBinding elementBinding = binding as ElementBinding;
 
-            string name = parameter.Name;
+			string name = parameter.Name;
 
-            string parameterType = def.GetDataType().TypeId;
+			string parameterType = def.GetDataType().TypeId;
 
-            ForgeTypeId groupTypeId = def.GetGroupTypeId();
-            string parameterGroup = LabelUtils.GetLabelForGroup(groupTypeId);
+			ForgeTypeId groupTypeId = def.GetGroupTypeId();
+			string parameterGroup = LabelUtils.GetLabelForGroup(groupTypeId);
 
-            bool instance = elementBinding is InstanceBinding;
+			bool instance = elementBinding is InstanceBinding;
 
-            List<string> categories = new List<string>();
-            if (elementBinding?.Categories != null)
-            {
-                categories = elementBinding.Categories.Cast<Category>().Select(c => c.Name).ToList();
-            }
+			List<string> categories = new List<string>();
+			if (elementBinding?.Categories != null)
+			{
+				categories = elementBinding.Categories.Cast<Category>().Select(c => c.Name).ToList();
+			}
 
 
-            if (parameter is SharedParameterElement sharedParameterElement)
-            {
-                var guid = sharedParameterElement.GuidValue.ToString();
-                paramDef.Shared = true;
-                paramDef.CustomData.Add("GUID", guid);
+			if (parameter is SharedParameterElement sharedParameterElement)
+			{
+				var guid = sharedParameterElement.GuidValue.ToString();
+				paramDefinition.Shared = true;
+				paramDefinition.CustomData.Add("GUID", guid);
 
-            }
+			}
 
-            paramDef = new ParameterDefinition
-            {
-                Name = name,
-                ParameterType = parameterType,
-                ParameterGroup = parameterGroup,
-                Instance = instance,
-                Categories = categories,
-            };
+			paramDefinition = new ParameterDefinition
+			{
+				Name = name,
+				ParameterType = parameterType,
+				ParameterGroup = parameterGroup,
+				Instance = instance,
+				Categories = categories,
+			};
 
-            paramDef.SetIdentifiers(parameter);
-            paramDef.CopyParameters(parameter, settings.MappingSettings);
-            paramDef.SetProperties(parameter, settings.MappingSettings);
-        
-            refObjects.AddOrReplace(parameter.Id, paramDef);
-            return paramDef;
-        }
+			paramDefinition.SetIdentifiers(parameter);
+			paramDefinition.CopyParameters(parameter, settings.MappingSettings);
+			paramDefinition.SetProperties(parameter, settings.MappingSettings);
+		
+			refObjects.AddOrReplace(parameter.Id, paramDefinition);
+			return paramDefinition;
+		}
 
-        /***************************************************/
-    }
+		/***************************************************/
+	}
 }
