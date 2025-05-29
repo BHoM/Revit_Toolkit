36481307e891ecc7f8b603a8f1b7756691f54c25
Pawel Baran <pawel.baran@burohappold.com>
2025-05-29 22:51:48 +0200

    compliance fixed

--- Revit_Engine/Compute/FilterByRevitParameter.cs
+++ Revit_Engine/Compute/FilterByRevitParameter.cs
@@ -1,24 +1,24 @@
 ﻿/*
-* This file is part of the Buildings and Habitats object Model (BHoM)
-* Copyright (c) 2015 - 2025, the respective contributors. All rights reserved.
-*
-* Each contributor holds copyright over their respective contributions.
-* The project versioning (Git) records all such contribution source information.
-*                                           
-*                                                                              
-* The BHoM is free software: you can redistribute it and/or modify         
-* it under the terms of the GNU Lesser General Public License as published by  
-* the Free Software Foundation, either version 3.0 of the License, or          
-* (at your option) any later version.                                          
-*                                                                              
-* The BHoM is distributed in the hope that it will be useful,              
-* but WITHOUT ANY WARRANTY; without even the implied warranty of               
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
-* GNU Lesser General Public License for more details.                          
-*                                                                            
-* You should have received a copy of the GNU Lesser General Public License     
-* along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
-*/
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2025, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
 
 using BH.Engine.Verification;
 using BH.oM.Adapters.Revit.Parameters;
@@ -41,26 +41,26 @@ namespace BH.Engine.Adapters.Revit
         [Description("Filters a collection of BHoM objects by a single Revit parameter criterion.")]
         [Input("bHoMObjects", "The collection of BHoM objects to filter.")]
         [Input("parameterName", "The name of the parameter to filter by.")]
-        [Input("filterType", "The type of filter to apply.")]
-        [Input("value", "The value to filter by.")]
+        [Input("referenceValue", "Reference value to compare the parameter value against.")]
+        [Input("comparisonType", "Comparison requirement, i.e. whether the parameter value should be equal, greater, less than reference value etc.")]
         [Output("A list of BHoM objects that match the specified criteria.")]
-        public static IEnumerable<IBHoMObject> FilterByRevitParameter(this IEnumerable<IBHoMObject> bHoMObjects, string parameterName, ValueComparisonType comparisonType, object value)
+        public static IEnumerable<IBHoMObject> FilterByRevitParameter(this IEnumerable<IBHoMObject> bHoMObjects, string parameterName, object referenceValue, ValueComparisonType comparisonType)
         {
             return FilterByRevitParameters(bHoMObjects,
                 new List<string>() { parameterName },
-                new List<ValueComparisonType>() { comparisonType },
-                new List<object>() { value });
+                new List<object>() { referenceValue },
+                new List<ValueComparisonType>() { comparisonType });
         }
 
         /***************************************************/
 
         [Description("Filters a collection of BHoM objects by multiple Revit parameter criteria.")]
         [Input("bHoMObjects", "The collection of BHoM objects to filter.")]
-        [Input("parameterNames", "A list of Revit parameter names to filter by.")]
-        [Input("filterTypes", "The types of filtering to apply.")]
-        [Input("values", "The values to filter the parameters against.")]
+        [Input("parameterNames", "Names of the parameters to filter by.")]
+        [Input("referenceValues", "Reference values to compare the parameter values against.")]
+        [Input("comparisonTypes", "Comparison requirements, i.e. whether the parameter values should be equal, greater, less than reference value etc.")]
         [Output("A list of BHoM objects that match the specified criteria.")]
-        public static IEnumerable<IBHoMObject> FilterByRevitParameters(this IEnumerable<IBHoMObject> bHoMObjects, IEnumerable<string> parameterNames, IEnumerable<ValueComparisonType> comparisonTypes, IEnumerable<object> referenceValues)
+        public static IEnumerable<IBHoMObject> FilterByRevitParameters(this IEnumerable<IBHoMObject> bHoMObjects, IEnumerable<string> parameterNames, IEnumerable<object> referenceValues, IEnumerable<ValueComparisonType> comparisonTypes)
         {
             if (parameterNames == null || comparisonTypes == null || referenceValues == null)
             {
