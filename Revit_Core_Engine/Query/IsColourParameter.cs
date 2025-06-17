c16a317833359b2bad249bfe9779454d5e34afa3
Michal Pekacki <michal.pekacki@burohappold.com>
2025-06-17 11:17:39 +0200

    added AsValueString check

--- Revit_Core_Engine/Query/IsColourParameter.cs
+++ Revit_Core_Engine/Query/IsColourParameter.cs
@@ -37,7 +37,7 @@ namespace BH.Revit.Engine.Core
         [Output("colour", "True if the Revit parameter is a colour, otherwise false.")]
         public static bool IsColourParameter(this Parameter parameter)
         {
-            if (parameter?.StorageType != StorageType.Integer)
+            if (parameter?.StorageType != StorageType.Integer || parameter?.AsValueString() != null)
                 return false;
 
 #if REVIT2021
