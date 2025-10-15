474a397d16b4746c6406499e47d05cc8beb941aa
Pawel Baran <pawel.baran@burohappold.com>
2025-10-15 12:21:40 +0200

    compliance fixed

--- Revit_Core_Engine/Create/ElementId.cs
+++ Revit_Core_Engine/Create/ElementId.cs
@@ -1,14 +1,50 @@
-﻿using Autodesk.Revit.DB;
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2025, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using Autodesk.Revit.DB;
+using BH.oM.Base.Attributes;
+using System.ComponentModel;
 
 namespace BH.Revit.Engine.Core
 {
     public static partial class Create
     {
+        /***************************************************/
+        /****              Public methods               ****/
+        /***************************************************/
+
+        [Description("Creates an ElementId from an integer value.")]
+        [Input("id", "Integer value to create the ElementId from.")]
+        [Output("elementId", "ElementId created from the provided integer value.")]
         public static ElementId ElementId(int id)
         {
             return new ElementId(id);
         }
 
+        /***************************************************/
+
+        [Description("Creates an ElementId from a long integer value.")]
+        [Input("id", "Long integer value to create the ElementId from.")]
+        [Output("elementId", "ElementId created from the provided long integer value.")]
         public static ElementId ElementId(long id)
         {
 #if REVIT2022 || REVIT2023
@@ -17,5 +53,7 @@ namespace BH.Revit.Engine.Core
             return new ElementId(id);
 #endif
         }
+
+        /***************************************************/
     }
 }
