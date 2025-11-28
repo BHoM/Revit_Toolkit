be2e4c7e8f72b833cab04153564935dae6916a65
Michal Pekacki <michal.pekacki@burohappold.com>
2025-11-28 10:48:48 +0100

    Add . to output description

--- Revit_Core_Engine/Query/LocationPoint.cs
+++ Revit_Core_Engine/Query/LocationPoint.cs
@@ -35,7 +35,7 @@ namespace BH.Revit.Engine.Core
         [Description("Returns the 3D location point of a given Revit element. The method determines the most appropriate point based on the element type, such as the spatial element calculation point, the element's location point, the midpoint of its location curve, or the center of its bounding box.")]
         [Input("element", "The Revit element from which to extract the location point.")]
         [Input("useRoomCalculationPoint", "If true and the element is a FamilyInstance with a spatial element calculation point, use that point as the location.")]
-        [Output("locationPoint", "The 3D point representing the location of the input Revit element")]
+        [Output("locationPoint", "The 3D point representing the location of the input Revit element.")]
         public static XYZ LocationPoint(this Element element, bool useRoomCalculationPoint = false)
         {
             if (element == null)
