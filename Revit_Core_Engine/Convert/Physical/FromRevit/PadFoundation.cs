f28c9fb622d7a5635845a11c7fb80f75633d0e22
wojciech buda <wojciech.buda@burohappold.com>
2026-03-17 15:31:10 +0100

    Pull update

--- Revit_Core_Engine/Convert/Physical/FromRevit/PadFoundation.cs
+++ Revit_Core_Engine/Convert/Physical/FromRevit/PadFoundation.cs
@@ -38,11 +38,11 @@ namespace BH.Revit.Engine.Core
         /****               Public Methods              ****/
         /***************************************************/
 
-        [Description("Converts a Revit FamilyInstance to BH.oM.Physical.Elements..")]
+        [Description("Converts a Revit FamilyInstance to BH.oM.Physical.Elements.PadFoundation.")]
         [Input("familyInstance", "Revit FamilyInstance to be converted.")]
         [Input("settings", "Revit adapter settings to be used while performing the convert.")]
         [Input("refObjects", "Optional, a collection of objects already processed in the current adapter action, stored to avoid processing the same object more than once.")]
-        [Output("padFoundation", "BH.oM.Physical.Elements.Pile resulting from converting the input Revit FamilyInstance.")]
+        [Output("padFoundation", "BH.oM.Physical.Elements.PadFoundation resulting from converting the input Revit FamilyInstance.")]
         public static PadFoundation PadFoundationFromRevit(this FamilyInstance familyInstance, RevitSettings settings = null, Dictionary<string, List<IBHoMObject>> refObjects = null)
         {
             settings = settings.DefaultIfNull();
@@ -57,12 +57,33 @@ namespace BH.Revit.Engine.Core
                 BH.Engine.Base.Compute.RecordError($"Failed to extract geometry from pad foundation. ElementId: {familyInstance.Id.Value()}");
                 return null;
             }
-                      
+
+            oM.Geometry.PlanarSurface planarSurface = null;
+
+            if (locationCurve is oM.Geometry.PlanarSurface ps)
+            {
+                planarSurface = ps;
+            }
+            else if (locationCurve is oM.Geometry.Polyline polyline)
+            {
+                planarSurface = BH.Engine.Geometry.Create.PlanarSurface(polyline);
+            }
+            else
+            {
+                BH.Engine.Base.Compute.RecordError(
+                    $"Unsupported geometry type for PadFoundation: {locationCurve.GetType().Name}"
+                );
+                return null;
+            }
+
             BoundingBoxXYZ bbox = familyInstance.get_BoundingBox(null);
             double depth = bbox.Max.Z - bbox.Min.Z;
 
-            padFoundation = BH.Engine.Physical.Create.PadFoundation((oM.Geometry.PlanarSurface)locationCurve, null, familyInstance.FamilyTypeFullName());
-                                
+            FamilySymbol familySymbol = familyInstance.Document.GetElement(familyInstance.GetTypeId()) as FamilySymbol;
+            oM.Physical.Constructions.IConstruction construction = familySymbol?.ConstructionFromRevit(settings, refObjects);
+
+            padFoundation = BH.Engine.Physical.Create.PadFoundation(planarSurface, construction, familyInstance.FamilyTypeFullName());
+
             //Set identifiers, parameters & custom data
             padFoundation.SetIdentifiers(familyInstance);
             padFoundation.CopyParameters(familyInstance, settings.MappingSettings);
