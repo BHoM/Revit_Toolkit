10201154ea381c0eaf5edb5b684ea0b3cdcf73e0
wojciech buda <wojciech.buda@burohappold.com>
2026-07-13 15:00:23 +0200

    Pull of PileFoundation added

--- Revit_Core_Engine/Query/BHoMType.cs
+++ Revit_Core_Engine/Query/BHoMType.cs
@@ -95,13 +95,13 @@ namespace BH.Revit.Engine.Core
                         BoundingBoxXYZ bbox = familyInstance.get_BoundingBox(null);
                         double width = Math.Max(bbox.Max.X - bbox.Min.X, bbox.Max.Y - bbox.Min.Y);
                         double height = bbox.Max.Z - bbox.Min.Z;
-                        if (height < width)
+                        if (familyInstance.GetSubComponentIds().Count != 0)
+                            return typeof(BH.oM.Physical.Elements.PileFoundation);
+                        else if (height < width)
                             return typeof(BH.oM.Physical.Elements.PadFoundation);
-                        else if (familyInstance.GetSubComponentIds().Count != 0)
-                            //return typeof(BH.oM.Physical.Elements.PileFoundation);
-                            return null; // Temporarily disabled until PileFoundation is properly implemented in the Revit adapter.
-                        else
-                            return typeof(BH.oM.Physical.Elements.Pile);
+                        return null; // Temporarily disabled until PileFoundation is properly implemented in the Revit adapter.
+                        //else
+                        //    return typeof(BH.oM.Physical.Elements.Pile);
                     }
                     if (typeof(BH.oM.Physical.Elements.Window).BuiltInCategories().Contains(category))
                         return typeof(BH.oM.Physical.Elements.Window);
